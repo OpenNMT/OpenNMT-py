90f5d2a7767b8574d37ad232655225899082bb72
Guillaume Klein <guillaumekln@users.noreply.github.com>
2017-05-31 09:47:46 +0200

    Cleanup trailing whitespaces

--- preprocess.py
+++ preprocess.py
@@ -148,7 +148,7 @@ def makeData(srcFile, tgtFile, srcDicts, tgtDicts):
             sizes += [len(srcWords)]            
         else:
             ignored += 1
-            
+
         count += 1
 
         if count % opt.report_every == 0:
