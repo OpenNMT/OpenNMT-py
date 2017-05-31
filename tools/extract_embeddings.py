227d5c4876312a560822527ba39131281fba8f1b
Sasha Rush <srush@seas.harvard.edu>
2017-05-31 12:01:41 -0400

    .

--- tools/extract_embeddings.py
+++ tools/extract_embeddings.py
@@ -13,13 +13,15 @@ parser.add_argument('-output_dir', default='.',
 parser.add_argument('-gpu', type=int, default=-1,
                     help="Device to run on")
 
+
 def write_embeddings(filename, dict, embeddings):
     with open(filename, 'w') as file:
         for i in range(len(embeddings)):
             str = dict.idxToLabel[i].encode("utf-8")
             for j in range(len(embeddings[0])):
-                str = str + " %5f"%(embeddings[i][j])
+                str = str + " %5f" % (embeddings[i][j])
             file.write(str + "\n")
+
 
 def main():
     opt = parser.parse_args()
@@ -36,16 +38,18 @@ def main():
     decoder = onmt.Models.Decoder(model_opt, tgt_dict)
     encoder_embeddings = encoder.word_lut.weight.data.tolist()
     decoder_embeddings = decoder.word_lut.weight.data.tolist()
-        
+
     print("Writing source embeddings")
-    write_embeddings(opt.output_dir + "/src_embeddings.txt", src_dict, encoder_embeddings)
+    write_embeddings(opt.output_dir + "/src_embeddings.txt", src_dict,
+                     encoder_embeddings)
 
     print("Writing target embeddings")
-    write_embeddings(opt.output_dir + "/tgt_embeddings.txt", tgt_dict, decoder_embeddings)
+    write_embeddings(opt.output_dir + "/tgt_embeddings.txt", tgt_dict,
+                     decoder_embeddings)
 
     print('... done.')
-    
     print('Converting model...')
+
 
 if __name__ == "__main__":
     main()
