966924661df05582ef743d922b5de52ea7c7d352
Sasha Rush <srush@seas.harvard.edu>
2017-06-02 12:12:24 -0400

    .

--- onmt/Models.py
+++ onmt/Models.py
@@ -107,6 +107,7 @@ class Decoder(nn.Module):
             emb_t = emb_t.squeeze(0)
             if self.input_feed:
                 emb_t = torch.cat([emb_t, output], 1)
+
             output, hidden = self.rnn(emb_t, hidden)
             output, attn = self.attn(output, context.t())
             output = self.dropout(output)
