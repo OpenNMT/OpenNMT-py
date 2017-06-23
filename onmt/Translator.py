4087c4610d89b74ba85d63c05be9e7d45e822f63
shuoyangd <shuoyangd@users.noreply.github.com>
2017-06-23 11:57:51 -0400

    loading model to the right place & fix encoding problem (#71)

--- onmt/Translator.py
+++ onmt/Translator.py
@@ -19,7 +19,8 @@ class Translator(object):
         self.tt = torch.cuda if opt.cuda else torch
         self.beam_accum = None
 
-        checkpoint = torch.load(opt.model)
+        checkpoint = torch.load(opt.model,
+                                map_location=lambda storage, loc: storage)
 
         model_opt = checkpoint['opt']
         self.src_dict = checkpoint['dicts']['src']
