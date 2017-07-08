0814e27ebb95f0278d2536f18fe670956b34f1fb
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-07-08 19:16:32 +0200

    Fixed line incompatible with python3

--- translate.py
+++ translate.py
@@ -116,9 +116,14 @@ def main():
 
         for b in range(len(predBatch)):
             count += 1
-            #outF.write(" ".join([i.decode('utf-8')
-            #                     for i in predBatch[b][0]]) + '\n')
-            outF.write(" ".join(predBatch[b][0]) + '\n') # can't do .decode on a str object
+            # a temporary fix at least
+            try:
+                # python2
+                outF.write(" ".join([i.decode('utf-8')
+                                 for i in predBatch[b][0]]) + '\n')
+            except AttributeError:
+                # python3: can't do .decode on a str object
+                outF.write(" ".join(predBatch[b][0]) + '\n')
             outF.flush()
 
             if opt.verbose:
