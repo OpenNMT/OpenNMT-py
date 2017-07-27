cd56947f43a9d15e0775ec5b54c4677b79228055
Alexander Rush <srush@sum1gpu01.rc.fas.harvard.edu>
2017-07-27 13:40:46 -0400

    .

--- onmt/modules/Embeddings.py
+++ onmt/modules/Embeddings.py
@@ -22,6 +22,7 @@ class Embeddings(nn.Module):
         # Word embeddings.
         self.dropout = nn.Dropout(p=opt.dropout)
         self.feature_dicts = feature_dicts
+    
         # Feature embeddings.
         if self.feature_dicts:
             self.feature_luts = nn.ModuleList([
@@ -71,7 +72,6 @@ class Embeddings(nn.Module):
             # Apply one MLP layer.
             emb = self.activation(
                 self.linear(torch.cat([word] + features, -1)))
-
         if self.positional_encoding:
             emb = emb + Variable(self.pe[:emb.size(0), :1, :emb.size(2)]
                                  .expand_as(emb))
