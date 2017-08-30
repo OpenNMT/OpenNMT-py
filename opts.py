46314ded344d034e73e7e4eda58e8714f1446aec
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-08-30 13:10:03 +0200

    Add pretrained/fixed emb options

--- opts.py
+++ opts.py
@@ -31,6 +31,20 @@ def model_opts(parser):
                         help='Use a sin to mark relative words positions.')
     parser.add_argument('-share_decoder_embeddings', action='store_true',
                         help='Share the word and out embeddings for decoder.')
+    parser.add_argument('-pre_word_vecs_enc',
+                        help="""If a valid path is specified, then this will load
+                        pretrained word embeddings on the encoder side.
+                        See README for specific formatting instructions.""")
+    parser.add_argument('-pre_word_vecs_dec',
+                        help="""If a valid path is specified, then this will load
+                        pretrained word embeddings on the decoder side.
+                        See README for specific formatting instructions.""")
+    parser.add_argument('-fix_word_vecs_enc',
+                        action='store_true',
+                        help="Fix word embeddings on the encoder side.")
+    parser.add_argument('-fix_word_vecs_dec',
+                        action='store_true',
+                        help="Fix word embeddings on the encoder side.")
 
     # RNN Options
     parser.add_argument('-encoder_type', type=str, default='rnn',
