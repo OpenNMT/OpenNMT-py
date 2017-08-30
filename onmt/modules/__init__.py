7541a126af73b0fa3e8afd2fccdad9afa471e143
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-08-30 14:30:38 +0200

    add Embeddings to modules

--- onmt/modules/__init__.py
+++ onmt/modules/__init__.py
@@ -7,10 +7,11 @@ from onmt.modules.StructuredAttention import MatrixTree
 from onmt.modules.Transformer import TransformerEncoder, TransformerDecoder
 from onmt.modules.MultiHeadedAttn import MultiHeadedAttention
 from onmt.modules.StackedRNN import StackedLSTM, StackedGRU
+from onmt.modules.Embeddings import Embeddings
 
 
 # For flake8 compatibility.
 __all__ = [GlobalAttention, ImageEncoder, CopyGenerator, MultiHeadedAttention,
            LayerNorm, Bottle, BottleLinear, BottleLayerNorm, BottleSoftmax,
-           TransformerEncoder, TransformerDecoder,
+           TransformerEncoder, TransformerDecoder, Embeddings,
            CopyCriterion, MatrixTree, StackedLSTM, StackedGRU, aeq]
