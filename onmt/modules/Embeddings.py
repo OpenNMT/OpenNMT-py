44314a73a3e4089975a9e0bb66b7b3991dffe012
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-09-01 00:07:29 +0200

    move Elementwise to Util

--- onmt/modules/Embeddings.py
+++ onmt/modules/Embeddings.py
@@ -1,38 +1,8 @@
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-from onmt.modules import BottleLinear
+from onmt.modules import BottleLinear, Elementwise
 from onmt.modules import aeq
-
-
-class Elementwise(nn.ModuleList):
-    """
-    A simple network container.
-    Parameters are a list of modules.
-    Inputs are a 3d Variable whose last dimension is the same length
-    as the list.
-    Outputs are the result of applying modules to inputs elementwise.
-    An optional merge parameter allows the outputs to be reduced to a
-    single Variable.
-    """
-
-    def __init__(self, merge=None, *args):
-        assert merge in [None, 'first', 'concat', 'sum', 'mlp']
-        self.merge = merge
-        super(Elementwise, self).__init__(*args)
-
-    def forward(self, input):
-        inputs = [feat.squeeze(2) for feat in input.split(1, dim=2)]
-        assert len(self) == len(inputs)
-        outputs = [f(x) for f, x in zip(self, inputs)]
-        if self.merge == 'first':
-            return outputs[0]
-        elif self.merge == 'concat' or self.merge == 'mlp':
-            return torch.cat(outputs, 2)
-        elif self.merge == 'sum':
-            return sum(outputs)
-        else:
-            return outputs
 
 
 class PositionalEncoding(nn.Module):
