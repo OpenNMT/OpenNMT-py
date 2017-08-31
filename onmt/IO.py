9d699c7d71c4f6add54676cb0bdcd500bded4b9d
srush <srush@seas.harvard.edu>
2017-08-31 09:29:15 -0400

    Update IO.py

--- onmt/IO.py
+++ onmt/IO.py
@@ -15,7 +15,7 @@ def __getstate__(self):
     return dict(self.__dict__, stoi=dict(self.stoi))
 
 
-def __setstate__(self, state): 
+def __setstate__(self, state):
     self.__dict__.update(state)
     self.stoi = defaultdict(lambda: 0, self.stoi)
 
