895a5cc15faaccf0b9b7b5ad06cfa6e58a6b0823
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-09-02 14:50:39 +0200

    remove all camelCase, simplify some alignment creation

--- onmt/IO.py
+++ onmt/IO.py
@@ -289,8 +289,8 @@ class ONMTDataset(torchtext.data.Dataset):
             src_size = max([t.size(0) for t in data])
             src_vocab_size = max([t.max() for t in data]) + 1
             alignment = torch.zeros(src_size, len(data), src_vocab_size)
-            for i in range(len(data)):
-                for j, t in enumerate(data[i]):
+            for i, sent in enumerate(data):
+                for j, t in enumerate(sent):
                     alignment[j, i, t] = 1
             return alignment
 
@@ -301,8 +301,8 @@ class ONMTDataset(torchtext.data.Dataset):
         def make_tgt(data, _):
             tgt_size = max([t.size(0) for t in data])
             alignment = torch.zeros(tgt_size, len(data)).long()
-            for i in range(len(data)):
-                alignment[:data[i].size(0), i] = data[i]
+            for i, sent in enumerate(data):
+                alignment[:sent.size(0), i] = sent
             return alignment
 
         fields["alignment"] = torchtext.data.Field(
