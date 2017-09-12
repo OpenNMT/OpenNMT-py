7836e350edcdc68843314f2e7ff3b0bcbb877e1d
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-09-12 14:23:56 +0200

    add use_gpu function

--- onmt/Utils.py
+++ onmt/Utils.py
@@ -1,6 +1,8 @@
-
 def aeq(base, *rest):
     """ Assert the first arg equals to each of the rest."""
     for a in rest[:]:
         assert a == base, "base(" + str(base) \
             + ") doesn't equals to each of " + str(rest)
+
+def use_gpu(opt):
+    return len(opt.gpuid) > 0
