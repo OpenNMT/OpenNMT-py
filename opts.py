d807d289229448d060c8e7b4f4061d1478a24b65
Jianyu Zhan <nasa4836@gmail.com>
2017-12-18 15:36:05 +0800

    preprocess: add max_shard_size option for sharding large text corpus
    For text corpus of large volume, it will be divided into shards of
    this size to preprocess.If 0, the data will be handled as a whole.

--- opts.py
+++ opts.py
@@ -136,6 +136,12 @@ def preprocess_opts(parser):
     group.add_argument('-save_data', required=True,
                        help="Output file for the prepared data")
 
+    group.add_argument('-max_shard_size', type=int, default=0,
+                       help="""For text corpus of large volume, it will
+                       be divided into shards of this size to preprocess.
+                       If 0, the data will be handled as a whole.
+                       The unit is in bytes.""")
+
     # Dictionary options, for text corpus
 
     group = parser.add_argument_group('Vocab')
@@ -350,6 +356,7 @@ def translate_opts(parser):
     group.add_argument('-output', default='pred.txt',
                        help="""Path to output the predictions (each line will
                        be the decoded sequence""")
+
     # Options most relevant to summarization.
     group.add_argument('-dynamic_dict', action='store_true',
                        help="Create dynamic dictionaries")
