934d98f97b9072b59e948a6734af60c1dbbff7f9
Jianri Li <skywalker@postech.edu>
2017-12-22 20:16:50 +0900

    Fix incorrect perplexity reported when label smoothing enabled.

--- onmt/Loss.py
+++ onmt/Loss.py
@@ -5,7 +5,6 @@ This includes: LossComputeBase and the standard NMTLossCompute, and
                sharded loss compute stuff.
 """
 from __future__ import division
-import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
