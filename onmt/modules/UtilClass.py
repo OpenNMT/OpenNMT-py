d6f199163a3c2cf6a6bfc20048c47cfa547f0d7c
Alexander Rush <srush@seas.harvard.edu>
2018-03-14 17:09:52 -0400

    .

--- onmt/modules/UtilClass.py
+++ onmt/modules/UtilClass.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 class LayerNorm(nn.Module):
     def __init__(self, features, eps=1e-6):
-        super().__init__()
+        super(LayerNorm, self).__init__()
         self.a_2 = nn.Parameter(torch.ones(features))
         self.b_2 = nn.Parameter(torch.zeros(features))
         self.eps = eps
