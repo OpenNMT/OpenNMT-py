f2f312aea8d7ebc1a485f88ced23fdba291d1b92
Alexander Rush <srush@seas.harvard.edu>
2018-03-15 13:33:44 -0400

    .

--- onmt/ModelConstructor.py
+++ onmt/ModelConstructor.py
@@ -210,7 +210,7 @@ def make_base_model(model_opt, fields, gpu, checkpoint=None):
                 p.data.uniform_(-model_opt.param_init, model_opt.param_init)
             for p in generator.parameters():
                 p.data.uniform_(-model_opt.param_init, model_opt.param_init)
-        if  model_opt.param_init_glorot:
+        if model_opt.param_init_glorot:
             for p in model.parameters():
                 if p.dim() > 1:
                     xavier_uniform(p)
