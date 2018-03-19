d1d0f6c10d30e9e5d8acc20f19ad00e0e03538d8
pltrdy <pltrdy@gmail.com>
2018-03-19 11:05:59 +0100

    pep8

--- tools/remove_optim.py
+++ tools/remove_optim.py
@@ -3,9 +3,12 @@ import argparse
 import torch
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Removes the optim data of PyTorch models")
-    parser.add_argument("--model", "-m", help="The model filename (*.pt)", required=True)
-    parser.add_argument("--output", "-o", help="The output filename (*.pt)", required=True)
+    parser = argparse.ArgumentParser(
+        description="Removes the optim data of PyTorch models")
+    parser.add_argument("--model", "-m",
+                        help="The model filename (*.pt)", required=True)
+    parser.add_argument("--output", "-o",
+                        help="The output filename (*.pt)", required=True)
     opt = parser.parse_args()
 
     model = torch.load(opt.model)
