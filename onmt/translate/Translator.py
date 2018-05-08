59808f6d15a7aa2a5b75f9e7ec78aa2ff9b4fdf6
Nikhil Verma <nikhilweee@gmail.com>
2018-05-08 08:20:42 +0000

    -report_bleu works

--- onmt/translate/Translator.py
+++ onmt/translate/Translator.py
@@ -15,7 +15,7 @@ import onmt.opts
 
 def make_translator(opt, report_score=True, out_file=None):
     if out_file is None:
-        out_file = codecs.open(opt.output, 'w', 'utf-8')
+        out_file = codecs.open(opt.output, 'w+', 'utf-8')
 
     if opt.gpu > -1:
         torch.cuda.set_device(opt.gpu)
@@ -35,7 +35,7 @@ def make_translator(opt, report_score=True, out_file=None):
     kwargs = {k: getattr(opt, k)
               for k in ["beam_size", "n_best", "max_length", "min_length",
                         "stepwise_penalty", "block_ngram_repeat",
-                        "ignore_when_blocking", "dump_beam",
+                        "ignore_when_blocking", "dump_beam", "report_bleu",
                         "data_type", "replace_unk", "gpu", "verbose"]}
 
     translator = Translator(model, fields, global_scorer=scorer,
@@ -395,11 +395,14 @@ class Translator(object):
 
     def _report_bleu(self, tgt_path):
         import subprocess
-        path = os.path.split(os.path.realpath(__file__))[0]
+        base_dir = os.path.abspath(__file__ + "/../../..")
+        tgt_path = os.path.join(os.getcwd(), tgt_path)
+        # Rollback pointer to the beginning.
+        self.out_file.seek(0)
         print()
 
         res = subprocess.check_output("perl %s/tools/multi-bleu.perl %s"
-                                      % (path, tgt_path, self.output),
+                                      % (base_dir, tgt_path),
                                       stdin=self.out_file,
                                       shell=True).decode("utf-8")
 
