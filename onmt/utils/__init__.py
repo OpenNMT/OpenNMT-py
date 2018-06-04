f3f09887e82118536bedd9ab231f15eaabdb3d34
pltrdy <pltrdy@gmail.com>
2018-06-04 15:44:09 +0200

    Convertion to pytorch 0.4 compatible code

--- onmt/utils/__init__.py
+++ onmt/utils/__init__.py
@@ -5,5 +5,7 @@ from onmt.utils.statistics import Statistics
 from onmt.utils.multi_utils import is_master, multi_init, \
     all_reduce_and_rescale_tensors
 
+import onmt.utils.optimizers
+
 __all__ = ["aeq", "use_gpu", "ReportMgr", "build_report_manager", "Statistics",
            "is_master", "multi_init", "all_reduce_and_rescale_tensors"]
