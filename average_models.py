5b89fa5551b130efcffc13c5ebce954843af1d53
Vincent Nguyen <vince62s@yahoo.com>
2018-06-07 08:33:06 +0200

    fix average_model and move it to main dir

--- average_models.py
+++ average_models.py
@@ -16,7 +16,7 @@ def average_models(model_files):
         generator_weights = m['generator']
 
         if i == 0:
-            vocab, opt, epoch = m['vocab'], m['opt'], m['epoch']
+            vocab, opt = m['vocab'], m['opt']
             avg_model = model_weights
             avg_generator = generator_weights
         else:
@@ -26,7 +26,7 @@ def average_models(model_files):
             for (k, v) in avg_generator.items():
                 avg_generator[k].mul_(i).add_(generator_weights[k]).div_(i + 1)
 
-    final = {"vocab": vocab, "opt": opt, "epoch": epoch, "optim": None,
+    final = {"vocab": vocab, "opt": opt, "optim": None,
              "generator": avg_generator, "model": avg_model}
     return final
 
