7ef6420ddf8517c101ec09336eef021cfb2ab17f
Travis CI User <travis@example.org>
2018-06-09 08:14:11 +0000

    Update docs after building Travis build 1088 of OpenNMT/OpenNMT-py
    The docs were built from the branch 'master' against the commit
    1a28cafd6a12215143bec4b914a6d9f6856147c8.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/OpenNMT/OpenNMT-py/jobs/390051860.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy --built-docs docs/build/html/ .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
