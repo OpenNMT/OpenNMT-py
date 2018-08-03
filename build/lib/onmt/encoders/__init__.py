4e65148a21da139c7be559676b0d99840b26bc59
Liam <liangding.liam@gmail.com>
2018-08-03 12:02:13 +0800

    Redundant script
    Redundant script

--- build/lib/onmt/encoders/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Module defining encoders."""
-from onmt.encoders.encoder import EncoderBase
-from onmt.encoders.transformer import TransformerEncoder
-from onmt.encoders.rnn_encoder import RNNEncoder
-from onmt.encoders.cnn_encoder import CNNEncoder
-from onmt.encoders.mean_encoder import MeanEncoder
-
-__all__ = ["EncoderBase", "TransformerEncoder", "RNNEncoder", "CNNEncoder",
-           "MeanEncoder"]
