5d40dc338de8fbdd8de33a940ea9ca1ffe9d757f
Liam <liangding.liam@gmail.com>
2018-08-03 12:06:08 +0800

    Redundant script
    Redundant script

--- build/lib/onmt/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Module defining models."""
-from onmt.models.model_saver import build_model_saver, ModelSaver
-from onmt.models.model import NMTModel
-
-__all__ = ["build_model_saver", "ModelSaver",
-           "NMTModel", "check_sru_requirement"]
