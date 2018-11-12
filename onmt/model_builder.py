a9472944e15b19cedd7e5eada96aad13ee5ff503
moses <vince62s@yahoo.com>
2018-11-12 12:35:02 +0100

    Use legacy Pytorch LayerNorm instead of adhoc Class

--- onmt/model_builder.py
+++ onmt/model_builder.py
@@ -2,7 +2,7 @@
 This file is for models creation, which consults options
 and creates each encoder and decoder accordingly.
 """
-
+import re
 import torch
 import torch.nn as nn
 from torch.nn.init import xavier_uniform_
@@ -236,6 +236,15 @@ def build_base_model(model_opt, fields, gpu, checkpoint=None):
 
     # Load the model states from checkpoint or initialize them.
     if checkpoint is not None:
+        # This preserves backward-compat for models using customed layernorm
+        fix_bias = lambda s: re.sub(
+          r'(.*)\.layer_norm((_\d+)?)\.b_2', r'\1.layer_norm\2.bias', s)
+        fix_weight = lambda s: re.sub(
+          r'(.*)\.layer_norm((_\d+)?)\.a_2', r'\1.layer_norm\2.weight', s)
+        fix_key = lambda k: fix_weight(fix_bias(k))
+        # end of patch for backward compatibility
+
+        checkpoint['model'] = {fix_key(k): v for (k, v) in checkpoint['model'].items()}
         model.load_state_dict(checkpoint['model'], strict=False)
         generator.load_state_dict(checkpoint['generator'], strict=False)
     else:
--- onmt/modules/__init__.py
+++ onmt/modules/__init__.py
@@ -1,5 +1,5 @@
 """  Attention and normalization modules  """
-from onmt.modules.util_class import LayerNorm, Elementwise
+from onmt.modules.util_class import Elementwise
 from onmt.modules.gate import context_gate_factory, ContextGate
 from onmt.modules.global_attention import GlobalAttention
 from onmt.modules.conv_multi_step_attention import ConvMultiStepAttention
@@ -9,7 +9,7 @@ from onmt.modules.embeddings import Embeddings, PositionalEncoding
 from onmt.modules.weight_norm import WeightNormConv2d
 from onmt.modules.average_attn import AverageAttention
 
-__all__ = ["LayerNorm", "Elementwise", "context_gate_factory", "ContextGate",
+__all__ = ["Elementwise", "context_gate_factory", "ContextGate",
            "GlobalAttention", "ConvMultiStepAttention", "CopyGenerator",
            "CopyGeneratorLossCompute", "MultiHeadedAttention", "Embeddings",
            "PositionalEncoding", "WeightNormConv2d", "AverageAttention"]
