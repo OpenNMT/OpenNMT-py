c35c29685a1727a6238e9d16a6dda71d91d7a511
bpopeters <bpeters@coli.uni-saarland.de>
2018-12-13 20:48:14 +0000

    remove UNK, which is unused

--- onmt/inputters/__init__.py
+++ onmt/inputters/__init__.py
@@ -7,14 +7,14 @@ from onmt.inputters.inputter import make_features, collect_features, \
     get_num_features, load_fields_from_vocab, get_fields, OrderedIterator, \
     save_fields_to_vocab, build_dataset, build_vocab
 from onmt.inputters.dataset_base import DatasetBase, PAD_WORD, BOS_WORD, \
-    EOS_WORD, UNK
+    EOS_WORD
 from onmt.inputters.text_dataset import TextDataset, ShardedTextCorpusIterator
 from onmt.inputters.image_dataset import ImageDataset
 from onmt.inputters.audio_dataset import AudioDataset, \
     ShardedAudioCorpusIterator
 
 
-__all__ = ['PAD_WORD', 'BOS_WORD', 'EOS_WORD', 'UNK', 'DatasetBase',
+__all__ = ['PAD_WORD', 'BOS_WORD', 'EOS_WORD', 'DatasetBase',
            'make_features', 'collect_features', 'get_num_features',
            'load_fields_from_vocab', 'get_fields',
            'save_fields_to_vocab', 'build_dataset',
--- onmt/inputters/dataset_base.py
+++ onmt/inputters/dataset_base.py
@@ -7,7 +7,6 @@ import torchtext
 
 PAD_WORD = '<blank>'
 UNK_WORD = '<unk>'
-UNK = 0
 BOS_WORD = '<s>'
 EOS_WORD = '</s>'
 
