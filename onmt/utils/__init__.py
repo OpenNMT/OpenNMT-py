57763443eff20c1e9e3c60d1d7f7a92801e8168c
Vincent Nguyen <vince62s@yahoo.com>
2018-12-19 14:50:35 +0100

    Introducing Adafactor (#1124)
    * introducing Adafactor

--- onmt/utils/__init__.py
+++ onmt/utils/__init__.py
@@ -3,8 +3,8 @@ from onmt.utils.misc import aeq, use_gpu
 from onmt.utils.report_manager import ReportMgr, build_report_manager
 from onmt.utils.statistics import Statistics
 from onmt.utils.optimizers import build_optim, MultipleOptimizer, \
-    Optimizer
+    Optimizer, AdaFactor
 
 __all__ = ["aeq", "use_gpu", "ReportMgr",
            "build_report_manager", "Statistics",
-           "build_optim", "MultipleOptimizer", "Optimizer"]
+           "build_optim", "MultipleOptimizer", "Optimizer", "AdaFactor"]
--- onmt/utils/optimizers.py
+++ onmt/utils/optimizers.py
@@ -2,8 +2,11 @@
 import torch
 import torch.optim as optim
 from torch.nn.utils import clip_grad_norm_
-
 from onmt.utils import use_gpu
+import operator
+import functools
+from copy import copy
+from math import sqrt
 
 
 def build_optim(model, opt, checkpoint):
@@ -187,6 +190,10 @@ class Optimizer(object):
                 initial_accumulator_value=self.adagrad_accum)
         elif self.method == 'adadelta':
             self.optimizer = optim.Adadelta(params, lr=self.learning_rate)
+        elif self.method == 'adafactor':
+            self.optimizer = AdaFactor(params, non_constant_decay=True,
+                                       enable_factorization=True,
+                                       weight_decay=0)
         elif self.method == 'adam':
             self.optimizer = optim.Adam(params, lr=self.learning_rate,
                                         betas=self.betas, eps=1e-9)
@@ -233,7 +240,185 @@ class Optimizer(object):
 
         self.learning_rate = lr_scale * self.original_lr
         for group in self.optimizer.param_groups:
-            group['lr'] = self.learning_rate
+            if self.method != 'adafactor':
+                group['lr'] = self.learning_rate
             if self.max_grad_norm:
                 clip_grad_norm_(group['params'], self.max_grad_norm)
         self.optimizer.step()
+
+# Code below is an implementation of https://arxiv.org/pdf/1804.04235.pdf
+# inspired but modified from https://github.com/DeadAt0m/adafactor-pytorch
+
+
+class AdaFactor(torch.optim.Optimizer):
+
+    def __init__(self, params, lr=None, beta1=0.9, beta2=0.999, eps1=1e-30,
+                 eps2=1e-3, cliping_threshold=1, non_constant_decay=True,
+                 enable_factorization=True, ams_grad=True, weight_decay=0):
+
+        enable_momentum = beta1 != 0
+
+        if non_constant_decay:
+            ams_grad = False
+
+        defaults = dict(lr=lr, beta1=beta1, beta2=beta2, eps1=eps1,
+                        eps2=eps2, cliping_threshold=cliping_threshold,
+                        weight_decay=weight_decay, ams_grad=ams_grad,
+                        enable_factorization=enable_factorization,
+                        enable_momentum=enable_momentum,
+                        non_constant_decay=non_constant_decay)
+
+        super(AdaFactor, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(AdaFactor, self).__setstate__(state)
+
+    def _experimental_reshape(self, shape):
+        temp_shape = shape[2:]
+        if len(temp_shape) == 1:
+            new_shape = (shape[0], shape[1]*shape[2])
+        else:
+            tmp_div = len(temp_shape) // 2 + len(temp_shape) % 2
+            new_shape = (shape[0]*functools.reduce(operator.mul,
+                                                   temp_shape[tmp_div:], 1),
+                         shape[1]*functools.reduce(operator.mul,
+                                                   temp_shape[:tmp_div], 1))
+        return new_shape, copy(shape)
+
+    def _check_shape(self, shape):
+        '''
+        output1 - True - algorithm for matrix, False - vector;
+        output2 - need reshape
+        '''
+        if len(shape) > 2:
+            return True, True
+        elif len(shape) == 2:
+            return True, False
+        elif len(shape) == 2 and (shape[0] == 1 or shape[1] == 1):
+            return False, False
+        else:
+            return False, False
+
+    def _rms(self, x):
+        return sqrt(torch.mean(x.pow(2)))
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+
+                if grad.is_sparse:
+                    raise RuntimeError('Adam does not support sparse \
+                                       gradients, use SparseAdam instead')
+
+                is_matrix, is_need_reshape = self._check_shape(grad.size())
+                new_shape = p.data.size()
+                if is_need_reshape and group['enable_factorization']:
+                    new_shape, old_shape = \
+                        self._experimental_reshape(p.data.size())
+                    grad = grad.view(new_shape)
+
+                state = self.state[p]
+                if len(state) == 0:
+                    state['step'] = 0
+                    if group['enable_momentum']:
+                        state['exp_avg'] = torch.zeros(new_shape,
+                                                       dtype=torch.float32,
+                                                       device=p.grad.device)
+
+                    if is_matrix and group['enable_factorization']:
+                        state['exp_avg_sq_R'] = \
+                            torch.zeros((1, new_shape[1]),
+                                        dtype=torch.float32,
+                                        device=p.grad.device)
+                        state['exp_avg_sq_C'] = \
+                            torch.zeros((new_shape[0], 1),
+                                        dtype=torch.float32,
+                                        device=p.grad.device)
+                    else:
+                        state['exp_avg_sq'] = torch.zeros(new_shape,
+                                                          dtype=torch.float32,
+                                                          device=p.grad.device)
+                    if group['ams_grad']:
+                        state['exp_avg_sq_hat'] = \
+                            torch.zeros(new_shape, dtype=torch.float32,
+                                        device=p.grad.device)
+
+                if group['enable_momentum']:
+                    exp_avg = state['exp_avg']
+
+                if is_matrix and group['enable_factorization']:
+                    exp_avg_sq_r = state['exp_avg_sq_R']
+                    exp_avg_sq_c = state['exp_avg_sq_C']
+                else:
+                    exp_avg_sq = state['exp_avg_sq']
+
+                if group['ams_grad']:
+                    exp_avg_sq_hat = state['exp_avg_sq_hat']
+
+                state['step'] += 1
+                if group['lr'] is None:
+                    # default value from paper
+                    lr_t = min(1e-2, 1 / sqrt(state['step']))
+                    lr_t *= max(group['eps2'], self._rms(p.data))
+                else:
+                    lr_t = group['lr']
+
+                if group['enable_momentum']:
+                    if group['non_constant_decay']:
+                        beta1_t = group['beta1'] * \
+                                  (1 - group['beta1'] ** (state['step'] - 1)) \
+                                  / (1 - group['beta1'] ** state['step'])
+                    else:
+                        beta1_t = group['beta1']
+                    exp_avg.mul_(beta1_t).add_(1 - beta1_t, grad)
+
+                if group['non_constant_decay']:
+                    beta2_t = group['beta2'] * \
+                              (1 - group['beta2'] ** (state['step'] - 1)) / \
+                              (1 - group['beta2'] ** state['step'])
+                else:
+                    beta2_t = group['beta2']
+
+                if is_matrix and group['enable_factorization']:
+                    exp_avg_sq_r.mul_(beta2_t). \
+                        add_(1 - beta2_t, torch.sum(torch.mul(grad, grad).
+                                                    add_(group['eps1']),
+                                                    dim=0, keepdim=True))
+                    exp_avg_sq_c.mul_(beta2_t). \
+                        add_(1 - beta2_t, torch.sum(torch.mul(grad, grad).
+                                                    add_(group['eps1']),
+                                                    dim=1, keepdim=True))
+                    v = torch.mul(exp_avg_sq_c,
+                                  exp_avg_sq_r).div_(torch.sum(exp_avg_sq_r))
+                else:
+                    exp_avg_sq.mul_(beta2_t). \
+                        addcmul_(1 - beta2_t, grad, grad). \
+                        add_((1 - beta2_t)*group['eps1'])
+                    v = exp_avg_sq
+
+                g = grad
+                if group['enable_momentum']:
+                    g = torch.div(exp_avg, 1 - beta1_t ** state['step'])
+
+                if group['ams_grad']:
+                    torch.max(exp_avg_sq_hat, v, out=exp_avg_sq_hat)
+                    v = exp_avg_sq_hat
+                    u = torch.div(g, (torch.div(v, 1 - beta2_t **
+                                  state['step'])).sqrt().add_(group['eps1']))
+                else:
+                    u = torch.div(g, v.sqrt())
+
+                u.div_(max(1, self._rms(u) / group['cliping_threshold']))
+                p.data.add_(-lr_t * (u.view(old_shape) if is_need_reshape and
+                            group['enable_factorization'] else u))
+
+                if group['weight_decay'] != 0:
+                    p.data.add_(-group['weight_decay'] * lr_t, p.data)
+
+        return loss
