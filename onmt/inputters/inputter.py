ac7ab76c9b183aa6e6f14afd627811bdf4b3bd55
bpopeters <bpeters@coli.uni-saarland.de>
2018-12-26 21:12:03 -0800

    do not pop img or audio from build_vocab

--- onmt/inputters/inputter.py
+++ onmt/inputters/inputter.py
@@ -328,9 +328,6 @@ def build_vocab(train_dataset_files, fields, data_type, share_vocab,
     Returns:
         Dict of Fields
     """
-    # Prop src from field to get lower memory using when training with image
-    if data_type == 'img' or data_type == 'audio':
-        fields.pop("src")
     counters = {k: Counter() for k, v in chain.from_iterable(fields.values())}
 
     # Load vocabulary
