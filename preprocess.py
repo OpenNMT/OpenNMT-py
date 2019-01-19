076eb503fd124fd324968758f84ca04a8842971e
Dylan Flaute <dylan.flaute@gmail.com>
2019-01-19 16:47:09 -0500

    Better preprocess existing files error.

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,6 @@
 
 import configargparse
 import glob
-import sys
 import gc
 import os
 import codecs
@@ -21,12 +20,17 @@ import onmt.opts as opts
 def check_existing_pt_files(opt):
     """ Check if there are existing .pt files to avoid overwriting them """
     pattern = opt.save_data + '.{}*.pt'
+    files_exist = []
     for t in ['train', 'valid', 'vocab']:
         path = pattern.format(t)
         if glob.glob(path):
-            sys.stderr.write("Please backup existing pt files: %s, "
-                             "to avoid overwriting them!\n" % path)
-            sys.exit(1)
+            files_exist.append(path)
+
+    if files_exist:
+        raise FileExistsError(
+            "Cannot write files matching '{}' because they "
+            "already exist. Please backup and/or delete "
+            "the existing files.".format(", ".join(files_exist)))
 
 
 def parse_args():
