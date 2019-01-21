f761aeb0af6b12ab60480c9dc4056bdcc86afed8
Dylan Flaute <dylan.flaute@gmail.com>
2019-01-21 15:02:56 -0500

    Move collapse_copy_scores out of dataset/reader and into the only file that uses it.

--- onmt/datatypes/text_datatype.py
+++ onmt/datatypes/text_datatype.py
@@ -36,35 +36,6 @@ class TextDataReader(DataReaderBase):
         for i, seq in enumerate(sequences):
             yield {side: seq, "indices": i}
 
-    @staticmethod
-    def collapse_copy_scores(scores, batch, tgt_vocab, src_vocabs,
-                             batch_dim=1, batch_offset=None):
-        """
-        Given scores from an expanded dictionary
-        corresponeding to a batch, sums together copies,
-        with a dictionary word when it is ambiguous.
-        """
-        offset = len(tgt_vocab)
-        for b in range(scores.size(batch_dim)):
-            blank = []
-            fill = []
-            batch_id = batch_offset[b] if batch_offset is not None else b
-            index = batch.indices.data[batch_id]
-            src_vocab = src_vocabs[index]
-            for i in range(1, len(src_vocab)):
-                sw = src_vocab.itos[i]
-                ti = tgt_vocab.stoi[sw]
-                if ti != 0:
-                    blank.append(offset + i)
-                    fill.append(ti)
-            if blank:
-                blank = torch.Tensor(blank).type_as(batch.indices.data)
-                fill = torch.Tensor(fill).type_as(batch.indices.data)
-                score = scores[:, b] if batch_dim == 1 else scores[b]
-                score.index_add_(1, fill, score.index_select(1, blank))
-                score.index_fill_(1, blank, 1e-10)
-        return scores
-
 
 def text_sort_key(ex):
     if hasattr(ex, "tgt"):
