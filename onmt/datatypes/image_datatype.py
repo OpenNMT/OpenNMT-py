775701ba5372cec20e4512527a97e4779643ccef
Dylan Flaute <dylan.flaute@gmail.com>
2019-01-21 13:48:11 -0500

    First basic coding of data readers.

--- onmt/datatypes/image_datatype.py
+++ onmt/datatypes/image_datatype.py
@@ -6,34 +6,53 @@ from onmt.datatypes.datareader_base import DataReaderBase
 
 
 class ImageDataReader(DataReaderBase):
+    """Create an image dataset reader.
+
+    Args:
+        truncate: maximum img size given as (rows, cols)
+            ((0,0) or None for unlimited)
+        channel_size (int): Number of image channels. Set to 1 for
+            grayscale.
+    """
+
+    def __init__(self, truncate=None, channel_size=3):
+        super(ImageDataReader, self).__init__()
+        self.truncate = truncate
+        self.channel_size = channel_size
+
+    @classmethod
+    def from_opt(cls, opt):
+        """Alternative constructor."""
+        return cls(channel_size=opt.image_channel_size)
 
     @staticmethod
     def sort_key(ex):
         """ Sort using the size of the image: (width, height)."""
         return ex.src.size(2), ex.src.size(1)
 
-    @classmethod
-    def make_examples(
-        cls, images, src_dir, side, truncate=None, channel_size=3
-    ):
-        """
+    def read(self, im_files, side, src_dir=None):
+        """Read images.
+
         Args:
-            path (str): location of a src file containing image paths
+            im_files (str, List[str]): Either a file of one file path per
+                line (either existing path or path relative to `src_dir`)
+                or a list thereof.
             src_dir (str): location of source images
             side (str): 'src' or 'tgt'
-            truncate: maximum img size ((0,0) or None for unlimited)
+
         Yields:
             a dictionary containing image data, path and index for each line.
         """
+
         from PIL import Image
         from torchvision import transforms
         import cv2
 
-        if isinstance(images, str):
-            images = cls._read_file(images)
+        if isinstance(im_files, str):
+            im_files = self._read_file(im_files)
 
-        for i, filename in enumerate(images):
-            filename = filename.strip()
+        for i, line in enumerate(im_files):
+            filename = line.strip()
             img_path = os.path.join(src_dir, filename)
             if not os.path.exists(img_path):
                 img_path = filename
@@ -41,13 +60,13 @@ class ImageDataReader(DataReaderBase):
             assert os.path.exists(img_path), \
                 'img path %s not found' % filename
 
-            if channel_size == 1:
+            if self.channel_size == 1:
                 img = transforms.ToTensor()(
                     Image.fromarray(cv2.imread(img_path, 0)))
             else:
                 img = transforms.ToTensor()(Image.open(img_path))
-            if truncate and truncate != (0, 0):
-                if not (img.size(1) <= truncate[0]
-                        and img.size(2) <= truncate[1]):
+            if self.truncate and self.truncate != (0, 0):
+                if not (img.size(1) <= self.truncate[0]
+                        and img.size(2) <= self.truncate[1]):
                     continue
             yield {side: img, side + '_path': filename, 'indices': i}
--- onmt/datatypes/text_datatype.py
+++ onmt/datatypes/text_datatype.py
@@ -9,16 +9,29 @@ class TextDataReader(DataReaderBase):
     """
     Build `Example` objects, `Field` objects, and filter_pred function
     from text corpus.
-
-    Args:
-        fields (dict): a dictionary of `torchtext.data.Field`.
-            Keys are like 'src', 'tgt', 'src_map', and 'alignment'.
-        src_examples_iter (dict iter): preprocessed source example
-            dictionary iterator.
-        tgt_examples_iter (dict iter): preprocessed target example
-            dictionary iterator.
-        dynamic_dict (bool)
     """
+
+    def read(self, sequences, side, src_dir=None):
+        """Read lines of text.
+
+        Args:
+            sequences: path to corpus file or iterable
+            side (str): "src" or "tgt".
+
+        Yields:
+            dictionaries whose keys are the names of fields and whose
+            values are more or less the result of tokenizing with those
+            fields.
+        """
+
+        assert src_dir is None or src_dir == '', \
+            "Cannot use src_dir with text dataset. (Got " \
+            "{:s}, but expected None or '')".format(src_dir.__str__())
+
+        if isinstance(sequences, str):
+            sequences = self._read_file(sequences)
+        for i, seq in enumerate(sequences):
+            yield {side: seq, "indices": i}
 
     @staticmethod
     def sort_key(ex):
@@ -54,21 +67,3 @@ class TextDataReader(DataReaderBase):
                 score.index_add_(1, fill, score.index_select(1, blank))
                 score.index_fill_(1, blank, 1e-10)
         return scores
-
-    @classmethod
-    def make_examples(cls, sequences, side):
-        """
-        Args:
-            sequences: path to corpus file or iterable
-            truncate (int): maximum sequence length (0 for unlimited).
-            side (str): "src" or "tgt".
-
-        Yields:
-            dictionaries whose keys are the names of fields and whose
-            values are more or less the result of tokenizing with those
-            fields.
-        """
-        if isinstance(sequences, str):
-            sequences = cls._read_file(sequences)
-        for i, seq in enumerate(sequences):
-            yield {side: seq, "indices": i}
