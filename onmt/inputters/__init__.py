2d859609882708d6db1baf3cb3b8aea0bd0c6781
Dylan Flaute <dylan.flaute@gmail.com>
2019-01-21 14:27:54 -0500

    Fix inputters/__init__.py

--- onmt/inputters/__init__.py
+++ onmt/inputters/__init__.py
@@ -7,13 +7,9 @@ from onmt.inputters.inputter import make_features, \
     load_old_vocab, get_fields, OrderedIterator, \
     build_dataset, build_vocab, old_style_vocab
 from onmt.inputters.dataset import DatasetBase
-from onmt.datatypes.text_datatype import TextDataset
-from onmt.datatypes.image_datatype import ImageDataset
-from onmt.datatypes.audio_datatype import AudioDataset
 
 
 __all__ = ['DatasetBase', 'make_features',
            'load_old_vocab', 'get_fields',
            'build_dataset', 'old_style_vocab',
-           'build_vocab', 'OrderedIterator',
-           'TextDataset', 'ImageDataset', 'AudioDataset']
+           'build_vocab', 'OrderedIterator']
