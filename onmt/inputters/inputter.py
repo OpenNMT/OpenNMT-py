3868cdc4933034872e52d5f4fac8fd37d55c5288
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-01-30 14:18:34 +0100

    Make batch size a multiple of 8 in case of FP16 training

--- onmt/inputters/inputter.py
+++ onmt/inputters/inputter.py
@@ -425,24 +425,70 @@ def _read_vocab_file(vocab_path, tag):
             return [line.strip().split()[0] for line in f if line.strip()]
 
 
+def batch_iter(data, batch_size, batch_size_fn=None, batch_size_multiple=1):
+    """Yield elements from data in chunks of batch_size, where each chunk size
+    is a multiple of batch_size_multiple.
+
+    This is an extended version of torchtext.data.batch.
+    """
+    if batch_size_fn is None:
+        def batch_size_fn(new, count, sofar):
+            return count
+    minibatch, size_so_far = [], 0
+    for ex in data:
+        minibatch.append(ex)
+        size_so_far = batch_size_fn(ex, len(minibatch), size_so_far)
+        if size_so_far >= batch_size:
+            overflowed = 0
+            if size_so_far > batch_size:
+                overflowed += 1
+            if batch_size_multiple > 1:
+                overflowed += (
+                    (len(minibatch) - overflowed) % batch_size_multiple)
+            if overflowed == 0:
+                yield minibatch
+                minibatch, size_so_far = [], 0
+            else:
+                yield minibatch[:-overflowed]
+                minibatch = minibatch[-overflowed:]
+                size_so_far = 0
+                for i, ex in enumerate(minibatch):
+                    size_so_far = batch_size_fn(ex, i + 1, size_so_far)
+    if minibatch:
+        yield minibatch
+
+
 class OrderedIterator(torchtext.data.Iterator):
+
+    def __init__(self,
+                 dataset,
+                 batch_size,
+                 batch_size_multiple=1,
+                 **kwargs):
+        super(OrderedIterator, self).__init__(dataset, batch_size, **kwargs)
+        self.batch_size_multiple = batch_size_multiple
 
     def create_batches(self):
         """ Create batches """
         if self.train:
             def _pool(data, random_shuffler):
                 for p in torchtext.data.batch(data, self.batch_size * 100):
-                    p_batch = torchtext.data.batch(
+                    p_batch = batch_iter(
                         sorted(p, key=self.sort_key),
-                        self.batch_size, self.batch_size_fn)
+                        self.batch_size,
+                        batch_size_fn=self.batch_size_fn,
+                        batch_size_multiple=self.batch_size_multiple)
                     for b in random_shuffler(list(p_batch)):
                         yield b
 
             self.batches = _pool(self.data(), self.random_shuffler)
         else:
             self.batches = []
-            for b in torchtext.data.batch(self.data(), self.batch_size,
-                                          self.batch_size_fn):
+            for b in batch_iter(
+                    self.data(),
+                    self.batch_size,
+                    batch_size_fn=self.batch_size_fn,
+                    batch_size_multiple=self.batch_size_multiple):
                 self.batches.append(sorted(b, key=self.sort_key))
 
 
@@ -457,11 +503,12 @@ class DatasetLazyIter(object):
     """
 
     def __init__(self, dataset_paths, fields, batch_size, batch_size_fn,
-                 device, is_train):
+                 batch_size_multiple, device, is_train):
         self._paths = dataset_paths
         self.fields = fields
         self.batch_size = batch_size
         self.batch_size_fn = batch_size_fn
+        self.batch_size_multiple = batch_size_multiple
         self.device = device
         self.is_train = is_train
 
@@ -473,8 +520,9 @@ class DatasetLazyIter(object):
                         (path, len(cur_dataset)))
             cur_dataset.fields = self.fields
             cur_iter = OrderedIterator(
-                dataset=cur_dataset,
-                batch_size=self.batch_size,
+                cur_dataset,
+                self.batch_size,
+                batch_size_multiple=self.batch_size_multiple,
                 batch_size_fn=self.batch_size_fn,
                 device=self.device,
                 train=self.is_train,
@@ -521,8 +569,9 @@ def build_dataset_iter(corpus_type, fields, opt, is_train=True):
     dataset_paths = sorted(glob.glob(opt.data + '.' + corpus_type + '*.pt'))
     batch_size = opt.batch_size if is_train else opt.valid_batch_size
     batch_fn = max_tok_len if is_train and opt.batch_type == "tokens" else None
+    batch_size_multiple = 8 if opt.model_dtype == "fp16" else 1
 
     device = "cuda" if opt.gpu_ranks else "cpu"
 
     return DatasetLazyIter(dataset_paths, fields, batch_size, batch_fn,
-                           device, is_train)
+                           batch_size_multiple, device, is_train)
