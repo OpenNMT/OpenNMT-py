ab47ddfa553a936b17871d7cb88bf0dffa405357
François Hernandez <francois.hernandez.fh@gmail.com>
2019-02-04 15:29:39 +0100

    pass type arg in transformer encoder self_attn

--- onmt/encoders/transformer.py
+++ onmt/encoders/transformer.py
@@ -43,7 +43,7 @@ class TransformerEncoderLayer(nn.Module):
         """
         input_norm = self.layer_norm(inputs)
         context, _ = self.self_attn(input_norm, input_norm, input_norm,
-                                    mask=mask)
+                                    mask=mask, type="self")
         out = self.dropout(context) + inputs
         return self.feed_forward(out)
 
