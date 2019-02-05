f79ce80d4cc4f4aab0c39079d89514647452b034
François Hernandez <francois.hernandez.fh@gmail.com>
2019-02-05 09:49:24 +0100

    relative positions matrix for non cache (training)

--- onmt/modules/multi_headed_attn.py
+++ onmt/modules/multi_headed_attn.py
@@ -170,13 +170,14 @@ class MultiHeadedAttention(nn.Module):
 
         if self.max_relative_positions > 0 and type == "self":
             key_len = key.size(2)
-            # 1 x key_len
+            # 1 or key_len x key_len
             relative_positions_matrix = generate_relative_positions_matrix(
-                key_len, self.max_relative_positions)
-            # 1 x key_len x dim_per_head
+                key_len, self.max_relative_positions,
+                cache=True if layer_cache is not None else False)
+            #  1 or key_len x key_len x dim_per_head
             relations_keys = self.relative_positions_embeddings(
                 relative_positions_matrix.to(device))
-            # 1 x key_len x dim_per_head
+            #  1 or key_len x key_len x dim_per_head
             relations_values = self.relative_positions_embeddings(
                 relative_positions_matrix.to(device))
 
@@ -187,7 +188,7 @@ class MultiHeadedAttention(nn.Module):
 
         # 2) Calculate and scale scores.
         query = query / math.sqrt(dim_per_head)
-        # batch x num_heads x 1 x key_len
+        # batch x num_heads x query_len x key_len
         query_key = torch.matmul(query, key.transpose(2, 3))
 
         if self.max_relative_positions > 0 and type == "self":
--- onmt/utils/misc.py
+++ onmt/utils/misc.py
@@ -85,10 +85,16 @@ def set_random_seed(seed, is_cuda):
         torch.cuda.manual_seed(seed)
 
 
-def generate_relative_positions_matrix(length, max_relative_positions):
+def generate_relative_positions_matrix(length, max_relative_positions,
+                                       cache=False):
     """Generate the clipped relative positions matrix
        for a given length and maximum relative positions"""
-    distance_mat = torch.arange(-length+1, 1, 1).unsqueeze(0)
+    if cache:
+        distance_mat = torch.arange(-length+1, 1 , 1).unsqueeze(0)
+    else:
+        range_vec = torch.arange(length)
+        range_mat = range_vec.unsqueeze(-1).expand(-1, length).transpose(0, 1)
+        distance_mat = range_mat - range_mat.transpose(0, 1)
     distance_mat_clipped = torch.clamp(distance_mat,
                                        min=-max_relative_positions,
                                        max=max_relative_positions)
@@ -105,7 +111,8 @@ def relative_matmul(x, z, transpose):
     x_t = x.permute(2, 0, 1, 3)
     x_t_r = x_t.reshape(length, heads * batch_size, -1)
     if transpose:
-        x_tz_matmul = torch.matmul(x_t_r, z.transpose(-2, -1))
+        z_t = z.transpose(1, 2)
+        x_tz_matmul = torch.matmul(x_t_r, z_t)
     else:
         x_tz_matmul = torch.matmul(x_t_r, z)
     x_tz_matmul_r = x_tz_matmul.reshape(length, batch_size, heads, -1)
