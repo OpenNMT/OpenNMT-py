d167a9e431d71347815ca847f873ff0fdfcdd264
Dylan Flaute <dylan.flaute@gmail.com>
2019-02-07 15:00:46 -0500

    Move helper methods out of Dataset and document them.

--- onmt/inputters/dataset_base.py
+++ onmt/inputters/dataset_base.py
@@ -9,6 +9,56 @@ from torchtext.data import Example
 from torchtext.vocab import Vocab
 
 
+def _join_dicts(*args):
+    """
+    Args:
+        dictionaries with disjoint keys.
+
+    Returns:
+        a single dictionary that has the union of these keys.
+    """
+
+    return dict(chain(*[d.items() for d in args]))
+
+
+def _dynamic_dict(example, src_field, tgt_field):
+    """Create copy-vocab and numericalize with it.
+
+    In-place adds ``"src_map"`` to ``example``. That is the copy-vocab
+    numericalization of the tokenized ``example["src"]``. If ``example``
+    has a ``"tgt"`` key, adds ``"alignment"`` to example. That is the
+    copy-vocab numericalization of the tokenized ``example["tgt"]``. The
+    alignment has an initial and final UNK token to match the BOS and EOS
+    tokens.
+
+    Args:
+        example (dict): An example dictionary with a ``"src"`` key and
+            maybe a ``"tgt"`` key. (This argument changes in place!)
+        src_field (torchtext.data.Field): Field object.
+        tgt_field (torchtext.data.Field): Field object.
+
+    Returns:
+        torchtext.data.Vocab and ``example``, changed as described.
+    """
+
+    src = src_field.tokenize(example["src"])
+    # make a small vocab containing just the tokens in the source sequence
+    unk = src_field.unk_token
+    pad = src_field.pad_token
+    src_ex_vocab = Vocab(Counter(src), specials=[unk, pad])
+    unk_idx = src_ex_vocab.stoi[unk]
+    # Map source tokens to indices in the dynamic dict.
+    src_map = torch.LongTensor([src_ex_vocab.stoi[w] for w in src])
+    example["src_map"] = src_map
+
+    if "tgt" in example:
+        tgt = tgt_field.tokenize(example["tgt"])
+        mask = torch.LongTensor(
+            [unk_idx] + [src_ex_vocab.stoi[w] for w in tgt] + [unk_idx])
+        example["alignment"] = mask
+    return src_ex_vocab, example
+
+
 class Dataset(TorchtextDataset):
     """Contain data and process it.
 
@@ -50,13 +100,16 @@ class Dataset(TorchtextDataset):
             indicating whether to include that example in the dataset.
 
     Attributes:
-        src_vocabs (List): Used with dynamic dict/copy attention.
+        src_vocabs (List[torchtext.data.Vocab]): Used with dynamic dict/copy
+            attention. There is a very short vocab for each src example.
+            It contains just the source words, e.g. so that the generator can
+            predict to copy them.
     """
 
     def __init__(self, fields, readers, data, dirs, sort_key,
                  filter_pred=None):
         self.sort_key = sort_key
-        dynamic_dict = 'src_map' in fields and 'alignment' in fields
+        can_copy = 'src_map' in fields and 'alignment' in fields
 
         read_iters = [r.read(dat[1], dat[0], dir_) for r, dat, dir_
                       in zip(readers, data, dirs)]
@@ -64,14 +117,14 @@ class Dataset(TorchtextDataset):
         # self.src_vocabs is used in collapse_copy_scores and Translator.py
         self.src_vocabs = []
         examples = []
-        for ex_dict in starmap(self._join_dicts, zip(*read_iters)):
-            if dynamic_dict:
+        for ex_dict in starmap(_join_dicts, zip(*read_iters)):
+            if can_copy:
                 src_field = fields['src'][0][1]
                 tgt_field = fields['tgt'][0][1]
                 # this assumes src_field and tgt_field are both text
-                src_vocab, ex_dict = self._dynamic_dict(
+                src_ex_vocab, ex_dict = _dynamic_dict(
                     ex_dict, src_field.base_field, tgt_field.base_field)
-                self.src_vocabs.append(src_vocab)
+                self.src_vocabs.append(src_ex_vocab)
             ex_fields = {k: v for k, v in fields.items() if k in ex_dict}
             ex = Example.fromdict(ex_dict, ex_fields)
             examples.append(ex)
@@ -94,31 +147,3 @@ class Dataset(TorchtextDataset):
         if remove_fields:
             self.fields = []
         torch.save(self, path)
-
-    def _join_dicts(self, *args):
-        """
-        Args:
-            dictionaries with disjoint keys.
-
-        Returns:
-            a single dictionary that has the union of these keys.
-        """
-
-        return dict(chain(*[d.items() for d in args]))
-
-    def _dynamic_dict(self, example, src_field, tgt_field):
-        src = src_field.tokenize(example["src"])
-        # make a small vocab containing just the tokens in the source sequence
-        unk = src_field.unk_token
-        pad = src_field.pad_token
-        src_vocab = Vocab(Counter(src), specials=[unk, pad])
-        # Map source tokens to indices in the dynamic dict.
-        src_map = torch.LongTensor([src_vocab.stoi[w] for w in src])
-        example["src_map"] = src_map
-
-        if "tgt" in example:
-            tgt = tgt_field.tokenize(example["tgt"])
-            mask = torch.LongTensor(
-                [0] + [src_vocab.stoi[w] for w in tgt] + [0])
-            example["alignment"] = mask
-        return src_vocab, example
