53f8cb94b019207ca49043db920e5644722d884b
Vincent Nguyen <vince62s@yahoo.com>
2019-02-09 18:22:15 +0100

    Bump version 0.8.0

--- onmt/translate/translator.py
+++ onmt/translate/translator.py
@@ -450,8 +450,6 @@ class Translator(object):
                     sampling_temp=self.random_sampling_temp,
                     keep_topk=self.sample_from_topk,
                     return_attention=attn_debug or self.replace_unk)
-            elif self.coverage_penalty is None:
-                return self._translate_batch_deprecated(batch, src_vocabs)
             else:
                 return self._translate_batch(
                     batch,
