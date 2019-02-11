0119a0f3e8137924a623bb9ba84d7382f7f234f1
Dylan Flaute <dylan.flaute@gmail.com>
2019-02-10 21:38:41 -0500

    Update docs, translation device selection, refactor translation strategies.

--- onmt/translate/beam_search.py
+++ onmt/translate/beam_search.py
@@ -1,7 +1,9 @@
 import torch
 
+from onmt.translate.decode_strategy import DecodeStrategy
 
-class BeamSearch(object):
+
+class BeamSearch(DecodeStrategy):
     """Generation beam search.
 
     Note that the attributes list is not exhaustive. Rather, it highlights
@@ -10,25 +12,20 @@ class BeamSearch(object):
     ``batch_size``).
 
     Args:
-        beam_size (int): Number of beams to use.
-        batch_size (int): Current batch size.
-        pad (int): Magic integer in output vocab.
-        bos (int): Magic integer in output vocab.
-        eos (int): Magic integer in output vocab.
+        beam_size (int): Number of beams to use (see base ``parallel_paths``).
+        batch_size (int): See base.
+        pad (int): See base.
+        bos (int): See base.
+        eos (int): See base.
         n_best (int): Don't stop until at least this many beams have
             reached EOS.
-        mb_device (torch.device or str): Device for memory bank (encoder).
+        mb_device (torch.device or str): See base ``device``.
         global_scorer (onmt.translate.GNMTGlobalScorer): Scorer instance.
-        min_length (int): Shortest acceptable generation, not counting
-            begin-of-sentence or end-of-sentence.
-        max_length (int): Longest acceptable sequence, not counting
-            begin-of-sentence (presumably there has been no EOS
-            yet if max_length is used as a cutoff).
-        return_attention (bool): Whether to work with attention too.
-        block_ngram_repeat (int): Block beams where
-            ``block_ngram_repeat``-grams repeat.
-        exclusion_tokens (set[str]): If a gram contains any of these
-            tokens, it may repeat.
+        min_length (int): See base.
+        max_length (int): See base.
+        return_attention (bool): See base.
+        block_ngram_repeat (int): See base.
+        exclusion_tokens (set[int]): See base.
         memory_lengths (torch.LongTensor): Lengths of encodings. Used for
             masking attentions.
 
@@ -36,19 +33,12 @@ class BeamSearch(object):
         top_beam_finished (torch.ByteTensor): Shape ``(B,)``.
         _batch_offset (torch.LongTensor): Shape ``(B,)``.
         _beam_offset (torch.LongTensor): Shape ``(batch_size x beam_size)``.
-        alive_seq (torch.LongTensor): Shape ``(B x beam_size, step)``. This
-            sequence grows in the ``step`` axis on each call to
-            :func:`advance()`.
+        alive_seq (torch.LongTensor): See base.
         topk_log_probs (torch.FloatTensor): Shape ``(B x beam_size,)``. These
             are the scores used for the topk operation.
-        alive_attn (torch.FloatTensor or NoneType): If tensor, shape is
-            ``(step, B x beam_size, inp_seq_len)``, where ``inp_seq_len``
-            is the (max) length of the input sequence.
         select_indices (torch.LongTensor or NoneType): Shape
             ``(B x beam_size,)``. This is just a flat view of the
             ``_batch_index``.
-        is_finished (torch.ByteTensor or NoneType): Shape
-            ``(B, beam_size)``. Initialized to ``None``.
         topk_scores (torch.FloatTensor): Shape
             ``(B, beam_size)``. These are the
             scores a sequence will receive if it finishes.
@@ -61,41 +51,24 @@ class BeamSearch(object):
             ``(1, B x beam_size, inp_seq_len)``.
         hypotheses (list[list[Tuple[torch.Tensor]]]): Contains a tuple
             of score (float), sequence (long), and attention (float or None).
-        scores (list[list[torch.FloatTensor]]): For each batch, holds a
-            list of beam scores.
-        predictions (list[list[torch.LongTensor]]): For each batch, holds a
-            list of beam prediction sequences.
-        attention (list[list[torch.FloatTensor or list[]]]): For each batch,
-            holds a list of beam attention sequence tensors (or empty lists)
-            having shape ``(step, inp_seq_len)`` where ``inp_seq_len`` is the
-            length of the sample (not the max length of all inp seqs).
     """
 
     def __init__(self, beam_size, batch_size, pad, bos, eos, n_best, mb_device,
                  global_scorer, min_length, max_length, return_attention,
                  block_ngram_repeat, exclusion_tokens, memory_lengths,
                  stepwise_penalty):
-        # magic indices
-        self.pad = pad
-        self.eos = eos
-        self.bos = bos
-
+        super(BeamSearch, self).__init__(
+            pad, bos, eos, batch_size, mb_device, beam_size, min_length,
+            block_ngram_repeat, exclusion_tokens, return_attention,
+            max_length)
         # beam parameters
-        self.min_length = min_length
         self.global_scorer = global_scorer
         self.beam_size = beam_size
-        self.max_length = max_length
-        self.return_attention = return_attention
         self.n_best = n_best
         self.batch_size = batch_size
-        self.block_ngram_repeat = block_ngram_repeat
-        self.exclusion_tokens = exclusion_tokens
 
         # result caching
         self.hypotheses = [[] for _ in range(batch_size)]
-        self.predictions = [[] for _ in range(batch_size)]
-        self.scores = [[] for _ in range(batch_size)]
-        self.attention = [[] for _ in range(batch_size)]
 
         # beam state
         self.top_beam_finished = torch.zeros([batch_size], dtype=torch.uint8)
@@ -103,15 +76,10 @@ class BeamSearch(object):
         self._beam_offset = torch.arange(
             0, batch_size * beam_size, step=beam_size, dtype=torch.long,
             device=mb_device)
-        self.alive_seq = torch.full(
-            [batch_size * beam_size, 1], self.bos, dtype=torch.long,
-            device=mb_device)
         self.topk_log_probs = torch.tensor(
             [0.0] + [float("-inf")] * (beam_size - 1), device=mb_device
         ).repeat(batch_size)
-        self.alive_attn = None
         self.select_indices = None
-        self.is_finished = None
         self._memory_lengths = memory_lengths
 
         # buffers for the topk scores and 'backpointer'
@@ -159,32 +127,13 @@ class BeamSearch(object):
                 _B, self.beam_size)
 
         # force the output to be longer than self.min_length
-        step = self.alive_seq.shape[1]
-        if step <= self.min_length:
-            log_probs[:, self.eos] = -1e20
+        step = len(self)
+        self.ensure_min_length(log_probs)
 
         # Multiply probs by the beam probability.
         log_probs += self.topk_log_probs.view(_B * self.beam_size, 1)
 
-        # block ngram repeats
-        if self.block_ngram_repeat > 0 and step > 1:
-            # iterate over all batches, over all beams
-            for bk in range(self.alive_seq.shape[0]):
-                hyp = self.alive_seq[bk, 1:]
-                ngrams = set()
-                fail = False
-                gram = []
-                for i in range(step - 1):
-                    # Last n tokens, n = block_ngram_repeat
-                    gram = (gram + [hyp[i].item()])[-self.block_ngram_repeat:]
-                    # skip the blocking if any token in gram is excluded
-                    if set(gram) & self.exclusion_tokens:
-                        continue
-                    if tuple(gram) in ngrams:
-                        fail = True
-                    ngrams.add(tuple(gram))
-                if fail:
-                    log_probs[bk] = -10e20
+        self.block_ngram_repeats(log_probs)
 
         # if the sequence ends now, then the penalty is the current
         # length + 1, to include the EOS token
@@ -242,8 +191,7 @@ class BeamSearch(object):
             self.topk_scores -= cov_penalty.view(_B, self.beam_size)
 
         self.is_finished = self.topk_ids.eq(self.eos)
-        if step == self.max_length:
-            self.is_finished.fill_(1)
+        self.ensure_max_length()
 
     def update_finished(self):
         # Penalize beams that finished.
