bdd33d93e960b4cf69cf184adf717e0c97d7b1f1
lordeddard <engg.ruthvik@gmail.com>
2019-03-08 12:10:42 +0530

    Option to give an existing vocab file(.pt or text) to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -136,8 +136,21 @@ def main(opt):
         logger.info("Building & saving validation data...")
         build_save_dataset('valid', fields, src_reader, tgt_reader, opt)
 
-    logger.info("Building & saving vocabulary...")
-    build_save_vocab(train_dataset_files, fields, opt)
+    if not opt.use_existing_vocab:
+        logger.info("Building & saving vocabulary...")
+        build_save_vocab(train_dataset_files, fields, opt)
+    else:
+        try:
+            logger.info("Using existing vocabulary...")
+            vocab = torch.load(opt.src_vocab)
+            # dump vocab with standard name
+            vocab_path = opt.save_data + '.vocab.pt'
+            torch.save(vocab, vocab_path)
+        except torch.serialization.pickle.UnpicklingError:
+            logger.info("Building vocab from text file...")
+            build_save_vocab([], fields, opt)
+
+
 
 
 def _get_parser():
