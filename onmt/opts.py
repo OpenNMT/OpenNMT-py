6bc8efe5282abe9aa72b18b87ee427856ac0f768
lordeddard <engg.ruthvik@gmail.com>
2019-03-11 19:34:06 +0530

    Using -src_vocab to pass vocab.pt files. (#1350)

--- onmt/opts.py
+++ onmt/opts.py
@@ -219,6 +219,8 @@ def preprocess_opts(parser):
     # Dictionary options, for text corpus
 
     group = parser.add_argument_group('Vocab')
+    # if you want to pass an existing vocab.pt file, pass it to
+    # -src_vocab alone as it already contains tgt vocab.
     group.add('--src_vocab', '-src_vocab', default="",
               help="Path to an existing source vocabulary. Format: "
                    "one word per line.")
