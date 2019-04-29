fdcd37333124e6447371ecb5e2bb88604bf3e692
bhushan <bhushan.s.94@gmail.com>
2019-04-29 14:41:21 -0700

    Text summarization

--- text_summarization_api.py
+++ text_summarization_api.py
@@ -1,8 +1,23 @@
-import argparse
-python translate.py -gpu 0  -batch_size 1  -beam_size 5  -model summary/gigaword_copy_acc_51.78_ppl_11.71_e20.pt  -src ./test.txt  -share_vocab  -output data/giga/test_1.pred  -min_length 6  -verbose  -stepwise_penalty  -coverage_penalty summary  -beta 5  -length_penalty wu  -alpha 0.9  -block_ngram_repeat 3  -ignore_when_blocking "."
+import os
 
+def summarize(input_file, use_gpu = False):
+  gpu_string = ""
+  print('IN Summarization')
+  if use_gpu:
+    gpu_string = "-gpu 0"
+  out_file = input_file + '_summarized.txt'
+  os.system("python translate.py {} -batch_size 1  -beam_size 5  -model summary/gigaword_copy_acc_51.78_ppl_11.71_e20.pt  -src {}  -share_vocab  -output {}  -min_length 6  -verbose  -stepwise_penalty  -coverage_penalty summary  -beta 5  -length_penalty wu  -alpha 0.9  -block_ngram_repeat 3  -ignore_when_blocking \".\" ".format(gpu_string, input_file, out_file))
+  
+  try:  
+    summarized = open(out_file, 'r')
+    sum_text = summarized.readlines()
+    summarized.close()
+    output   = ". ".join(sum_text).strip()
+    print(output)
+    summarized = open(out_file, 'w')
+    summarized.write(output)
+    summarized.close()
+  except FileNotFoundError:
+    print('Summarized file not found')
 
-
-def summarize(input_file):
-    parser = argparse.ArgumentParser(description='Processing Args')
-    
+summarize('test.txt')
