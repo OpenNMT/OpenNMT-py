b6c28c1911546418494aec185a9b83260ba4d37e
bhushan23 <bhushan.s.94@gmail.com>
2019-04-29 16:39:25 -0400

    testing

---
+++ test.sh
@@ -0,0 +1,1 @@
+python translate.py -gpu 0  -batch_size 1  -beam_size 5  -model summary/gigaword_copy_acc_51.78_ppl_11.71_e20.pt  -src ./test.txt  -share_vocab  -output data/giga/test_1.pred  -min_length 6  -verbose  -stepwise_penalty  -coverage_penalty summary  -beta 5  -length_penalty wu  -alpha 0.9  -block_ngram_repeat 3  -ignore_when_blocking "."
