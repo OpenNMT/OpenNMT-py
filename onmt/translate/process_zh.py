0b2def537eaff138e80b24d639cdbb93e9cc210b
linxiao zeng <419344739@qq.com>
2019-07-16 18:12:51 +0200

    fix function name

--- onmt/translate/process_zh.py
+++ onmt/translate/process_zh.py
@@ -4,30 +4,30 @@ import pkuseg
 
 
 # Chinese segmentation
-def ZH_segmentator(line):
+def zh_segmentator(line):
     return " ".join(pkuseg.pkuseg().cut(line))
 
 
 # Chinese simplify -> Chinese traditional standard
-def ZH_traditional_standard(line):
+def zh_traditional_standard(line):
     return HanLP.convertToTraditionalChinese(line)
 
 
 # Chinese simplify -> Chinese traditional (HongKong)
-def ZH_traditional_HK(line):
+def zh_traditional_HK(line):
     return HanLP.s2hk(line)
 
 
 # Chinese simplify -> Chinese traditional (Taiwan)
-def ZH_traditional_TW(line):
+def zh_traditional_TW(line):
     return HanLP.s2tw(line)
 
 
 # Chinese traditional -> Chinese simplify (v1)
-def ZH_simplify(line):
+def zh_simplify(line):
     return HanLP.convertToSimplifiedChinese(line)
 
 
 # Chinese traditional -> Chinese simplify (v2)
-def ZH_simplify_v2(line):
+def zh_simplify_v2(line):
     return SnowNLP(line).han
