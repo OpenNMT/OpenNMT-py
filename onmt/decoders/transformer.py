88e6cbbcc3df95ff5359705c1c6186fef4d054d1
François Hernandez <francois.hernandez.fh@gmail.com>
2019-08-21 10:17:13 +0200

    pytorch 1.2 compatibility - mask & bool tensor (#1527)

--- onmt/decoders/transformer.py
+++ onmt/decoders/transformer.py
@@ -68,6 +68,11 @@ class TransformerDecoderLayer(nn.Module):
                 device=tgt_pad_mask.device,
                 dtype=torch.uint8)
             future_mask = future_mask.triu_(1).view(1, tgt_len, tgt_len)
+            # BoolTensor was introduced in pytorch 1.2
+            try:
+                future_mask = future_mask.bool()
+            except AttributeError:
+                pass
             dec_mask = torch.gt(tgt_pad_mask + future_mask, 0)
 
         input_norm = self.layer_norm_1(inputs)
