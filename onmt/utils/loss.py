Fixing coverage loss computation (#1539)
Sum on all dimensions in order to get a scalar result instead of a tensor.

* supposed to fix https://github.com/OpenNMT/OpenNMT-py/issues/1528

(this PR is not much tested at the moment)

onmt/utils/loss.py
@@ -268,7 +268,7 @@ class NMTLossCompute(LossComputeBase):
         return loss, stats
 
     def _compute_coverage_loss(self, std_attn, coverage_attn):
-        covloss = torch.min(std_attn, coverage_attn).sum(2).view(-1)
+        covloss = torch.min(std_attn, coverage_attn).sum()
         covloss *= self.lambda_coverage
         return covloss
 