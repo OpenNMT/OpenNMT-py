c39f4915aeef5d49e999b1f31ae0eb3ebb9389cd
pltrdy <pltrdy@gmail.com>
2019-09-24 18:51:11 +0200

    fix scripts

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 from onmt.bin.preprocess import main
 
-main()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 from onmt.bin.server import main
 
-main()
+
+if __name__ == "__main__":
+    main()
