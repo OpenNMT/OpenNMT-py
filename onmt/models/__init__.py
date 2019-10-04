d9f993ee4d0307bff170611bd573e0ec12ad48a1
Yiheng Xu <github@ranpox.com>
2019-10-04 11:42:57 -0500

    fix models __init__ (#1556)
    * fix models __init__
    
    * remove check_sru_requirement from __all__

--- onmt/models/__init__.py
+++ onmt/models/__init__.py
@@ -2,5 +2,4 @@
 from onmt.models.model_saver import build_model_saver, ModelSaver
 from onmt.models.model import NMTModel
 
-__all__ = ["build_model_saver", "ModelSaver",
-           "NMTModel", "check_sru_requirement"]
+__all__ = ["build_model_saver", "ModelSaver", "NMTModel"]
