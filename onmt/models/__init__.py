a12bbfde563ab4664d813ac47c7bd125f2b517a7
Yiheng Xu <hi.ranpox@gmail.com>
2019-10-04 10:09:48 -0500

    remove check_sru_requirement from __all__

--- onmt/models/__init__.py
+++ onmt/models/__init__.py
@@ -1,7 +1,5 @@
 """Module defining models."""
 from onmt.models.model_saver import build_model_saver, ModelSaver
 from onmt.models.model import NMTModel
-from onmt.models.sru import check_sru_requirement
 
-__all__ = ["build_model_saver", "ModelSaver",
-           "NMTModel", "check_sru_requirement"]
+__all__ = ["build_model_saver", "ModelSaver", "NMTModel"]
