5e7ef4184e45bf31a27a002475200b722cec00b9
SteveKommrusch <steve.kommrusch@gmail.com>
2019-11-05 14:46:24 -0700

    Improved tree edge creation for equivalence problem.

--- onmt/encoders/ggnn_encoder.py
+++ onmt/encoders/ggnn_encoder.py
@@ -137,13 +137,15 @@ class GGNNEncoder(EncoderBase):
         self._check_args(src, lengths)
         # First length tells us how many nodes for this batch
         nodes = lengths[0].item()
+        if (nodes != self.n_node):
+            print("Error: nodes on input not equal to n_nodes for graph: ",nodes," != ",self.n_node,flush=True)
+            raise ValueError("Nodes in input not equal to n_nodes for training: ",nodes," != ",self.n_node)
 
-        if False: # torch.cuda.is_available():
-            # print("SJKDEBUG: cuda nodes,lengths,self.state_dim",nodes,lengths,self.state_dim,flush=True)
+        # FIXME: Perhaps this should be part of the model?
+        if torch.cuda.is_available():
             prop_state=torch.cuda.FloatTensor(src.size()[1],nodes,self.state_dim).fill_(0)
             A=torch.cuda.FloatTensor(src.size()[1],nodes,nodes*self.n_edge_types*2).fill_(0)
         else:
-            # print("SJKDEBUG: cpu nodes,lengths,self.state_dim",nodes,lengths,self.state_dim,flush=True)
             prop_state=torch.zeros(src.size()[1],nodes,self.state_dim)
             A=torch.zeros(src.size()[1],nodes,nodes*self.n_edge_types*2)
 
@@ -161,40 +163,140 @@ class GGNNEncoder(EncoderBase):
         elif True:
             # Hardcoded tree for linear algebra equivalence explorations
             for i in range(src.size()[1]):
-                for j in range(128):
+                for j in range(nodes-1):
                     # Initialize node state
                     # FIXME: There is probably a way to have an embedding layer here
                     prop_state[i][j][src[j][i]] = 1.0
+                    # print("SJKDEBUG i",i,"j",j," src j i",src[j][i])
+                # Last node is flagged as aggregator and left and right children are the programs
+                prop_state[i][nodes-1][self.state_dim-1] = 1.0
+                # For lin alg eq, edge types are 8 (left, right, LL, LR, RL, RR, 
+                # startprog, endprog) and we do bidirectional as 2nd half of A matrix
+                L_in = 0
+                R_in = nodes
+                LL_in = nodes*2
+                LR_in = nodes*3
+                RL_in = nodes*4
+                RR_in = nodes*5
+                Strt_in = nodes*6
+                End_in = nodes*7
+                L_out = nodes*8
+                R_out = nodes*9
+                LL_out = nodes*10
+                LR_out = nodes*11
+                RL_out = nodes*12
+                RR_out = nodes*13
+                Strt_out = nodes*14
+                End_out = nodes*15
+                # To help in learning, label nodes with tree location data
+                tree = 120 
+                A[i][nodes-1][Strt_in+0] = 1.0
+                A[i][0][Strt_out + nodes-1] = 1.0
+                A[i][nodes-1][End_in + 1] = 1.0
+                A[i][1][End_out + nodes-1] = 1.0
 
-                # For lin alg eq, edge types are 2 (left, right) and we do bidirectional
                 # Loop for both programs
                 for lvl0 in range(2):
                     # Root is lvl0 children at +2 and +64
-                    A[i][lvl0][lvl0+2] = 1.0
-                    A[i][lvl0+2][nodes + lvl0] = 1.0
-                    A[i][lvl0][nodes*2+lvl0+64] = 1.0
-                    A[i][lvl0+64][nodes*3 + lvl0] = 1.0
+                    # Check that children aren't Null (max of 126 entries means [127] is always Null value)
+                    prop_state[i][lvl0][lvl0+tree] = 1.0
+                    if (src[lvl0+2][i] != src[127][i]):
+                        A[i][lvl0][L_in+lvl0+2] = 1.0
+                        A[i][lvl0+2][L_out+ lvl0] = 1.0
+                    if (src[lvl0+64][i] != src[127][i]):
+                        A[i][lvl0][R_in+lvl0+64] = 1.0
+                        A[i][lvl0+64][R_out + lvl0] = 1.0
+                    if (src[lvl0+4][i] != src[127][i]):
+                        A[i][lvl0][LL_in+lvl0+4] = 1.0
+                        A[i][lvl0+4][LL_out+ lvl0] = 1.0
+                    if (src[lvl0+66][i] != src[127][i]):
+                        A[i][lvl0][RL_in+lvl0+66] = 1.0
+                        A[i][lvl0+66][RL_out + lvl0] = 1.0
+                    if (src[lvl0+34][i] != src[127][i]):
+                        A[i][lvl0][LR_in+lvl0+34] = 1.0
+                        A[i][lvl0+34][LR_out+ lvl0] = 1.0
+                    if (src[lvl0+96][i] != src[127][i]):
+                        A[i][lvl0][RR_in+lvl0+96] = 1.0
+                        A[i][lvl0+96][RR_out + lvl0] = 1.0
                     for lvl1 in (lvl0+2,lvl0+64):
                         # lvl1 is this node, children at +2 and +32
-                        A[i][lvl1][lvl1+2] = 1.0
-                        A[i][lvl1+2][nodes + lvl1] = 1.0
-                        A[i][lvl1][nodes*2+lvl1+32] = 1.0
-                        A[i][lvl1+32][nodes*3 + lvl1] = 1.0
+                        if (src[lvl1][i] == src[127][i]):
+                            continue
+                        prop_state[i][lvl1][lvl0+2+tree] = 1.0
+                        if (src[lvl1+2][i] != src[127][i]):
+                            A[i][lvl1][L_in + lvl1+2] = 1.0
+                            A[i][lvl1+2][L_out + lvl1] = 1.0
+                        if (src[lvl1+32][i] != src[127][i]):
+                            A[i][lvl1][R_in+lvl1+32] = 1.0
+                            A[i][lvl1+32][R_out + lvl1] = 1.0
+                        if (src[lvl1+4][i] != src[127][i]):
+                            A[i][lvl1][LL_in + lvl1+4] = 1.0
+                            A[i][lvl1+4][LL_out + lvl1] = 1.0
+                        if (src[lvl1+34][i] != src[127][i]):
+                            A[i][lvl1][RL_in+lvl1+34] = 1.0
+                            A[i][lvl1+34][RL_out + lvl1] = 1.0
+                        if (src[lvl1+18][i] != src[127][i]):
+                            A[i][lvl1][LR_in + lvl1+18] = 1.0
+                            A[i][lvl1+18][LR_out + lvl1] = 1.0
+                        if (src[lvl1+48][i] != src[127][i]):
+                            A[i][lvl1][RR_in+lvl1+48] = 1.0
+                            A[i][lvl1+48][RR_out + lvl1] = 1.0
                         for lvl2 in (lvl1+2,lvl1+32):
-                            A[i][lvl2][lvl2+2] = 1.0
-                            A[i][lvl2+2][nodes + lvl2] = 1.0
-                            A[i][lvl2][nodes*2+lvl2+16] = 1.0
-                            A[i][lvl2+16][nodes*3 + lvl2] = 1.0
+                            if (src[lvl2][i] == src[127][i]):
+                                continue
+                            prop_state[i][lvl2][lvl0+4+tree] = 1.0
+                            if (src[lvl2+2][i] != src[127][i]):
+                                A[i][lvl2][L_in + lvl2+2] = 1.0
+                                A[i][lvl2+2][L_out + lvl2] = 1.0
+                            if (src[lvl2+16][i] != src[127][i]):
+                                A[i][lvl2][R_in+lvl2+16] = 1.0
+                                A[i][lvl2+16][R_out + lvl2] = 1.0
+                            if (src[lvl2+4][i] != src[127][i]):
+                                A[i][lvl2][LL_in + lvl2+4] = 1.0
+                                A[i][lvl2+4][LL_out + lvl2] = 1.0
+                            if (src[lvl2+18][i] != src[127][i]):
+                                A[i][lvl2][RL_in+lvl2+18] = 1.0
+                                A[i][lvl2+18][RL_out + lvl2] = 1.0
+                            if (src[lvl2+10][i] != src[127][i]):
+                                A[i][lvl2][LR_in + lvl2+10] = 1.0
+                                A[i][lvl2+10][LR_out + lvl2] = 1.0
+                            if (src[lvl2+24][i] != src[127][i]):
+                                A[i][lvl2][RR_in+lvl2+24] = 1.0
+                                A[i][lvl2+24][RR_out + lvl2] = 1.0
                             for lvl3 in (lvl2+2,lvl2+16):
-                                A[i][lvl3][lvl3+2] = 1.0
-                                A[i][lvl3+2][nodes + lvl3] = 1.0
-                                A[i][lvl3][nodes*2+lvl3+8] = 1.0
-                                A[i][lvl3+8][nodes*3 + lvl3] = 1.0
+                                if (src[lvl3][i] == src[127][i]):
+                                    continue
+                                prop_state[i][lvl3][lvl0+6+tree] = 1.0
+                                if (src[lvl3+2][i] != src[127][i]):
+                                    A[i][lvl3][L_in + lvl3+2] = 1.0
+                                    A[i][lvl3+2][L_out + lvl3] = 1.0
+                                if (src[lvl3+8][i] != src[127][i]):
+                                    A[i][lvl3][R_in+lvl3+8] = 1.0
+                                    A[i][lvl3+8][R_out + lvl3] = 1.0
+                                if (src[lvl3+4][i] != src[127][i]):
+                                    A[i][lvl3][LL_in + lvl3+4] = 1.0
+                                    A[i][lvl3+4][LL_out + lvl3] = 1.0
+                                if (src[lvl3+10][i] != src[127][i]):
+                                    A[i][lvl3][RL_in+lvl3+10] = 1.0
+                                    A[i][lvl3+10][RL_out + lvl3] = 1.0
+                                if (src[lvl3+6][i] != src[127][i]):
+                                    A[i][lvl3][LR_in + lvl3+6] = 1.0
+                                    A[i][lvl3+6][LR_out + lvl3] = 1.0
+                                if (src[lvl3+12][i] != src[127][i]):
+                                    A[i][lvl3][RR_in+lvl3+12] = 1.0
+                                    A[i][lvl3+12][RR_out + lvl3] = 1.0
                                 for lvl4 in (lvl3+2,lvl3+8):
-                                    A[i][lvl4][lvl4+2] = 1.0
-                                    A[i][lvl4+2][nodes + lvl4] = 1.0
-                                    A[i][lvl4][nodes*2+lvl4+4] = 1.0
-                                    A[i][lvl4+4][nodes*3 + lvl4] = 1.0
+                                    if (src[lvl4][i] == src[127][i]):
+                                        continue
+                                    prop_state[i][lvl4][lvl0+8+tree] = 1.0
+                                    if (src[lvl4+2][i] != src[127][i]):
+                                        prop_state[i][lvl4+2][lvl0+10+tree] = 1.0
+                                        A[i][lvl4][L_in + lvl4+2] = 1.0
+                                        A[i][lvl4+2][L_out + lvl4] = 1.0
+                                    if (src[lvl4+4][i] != src[127][i]):
+                                        prop_state[i][lvl4+4][lvl0+10+tree] = 1.0
+                                        A[i][lvl4][R_in+lvl4+4] = 1.0
+                                        A[i][lvl4+4][R_out + lvl4] = 1.0
 
         for i_step in range(self.n_steps):
             in_states = []
@@ -219,7 +321,8 @@ class GGNNEncoder(EncoderBase):
             join_state = torch.stack((join_state,join_state,join_state,join_state))
         elif True:
             # Use roots of the 2 program trees as inputs to bridge
-            join_state = torch.stack((prop_state[0],prop_state[1],prop_state[1],prop_state[0]))
+            # print("SJKDEBUG propstate size",prop_state.size())
+            join_state = torch.stack((prop_state[nodes-1],prop_state[nodes-1],prop_state[nodes-1],prop_state[nodes-1]))
         join_state = (join_state,join_state)
 
         # print("SJKDEBUG:join_state",len(join_state),join_state[0].size())
@@ -251,7 +354,7 @@ class GGNNEncoder(EncoderBase):
             """
             size = states.size()
             result = linear(states.view(-1, self.total_hidden_dim))
-            return F.relu(result).view(size)
+            return F.leaky_relu(result).view(size)
 
         if isinstance(hidden, tuple):  # LSTM
             outs = tuple([bottle_hidden(layer, hidden[ix])
