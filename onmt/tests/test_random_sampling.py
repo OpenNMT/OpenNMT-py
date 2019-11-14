3c0e6df8bb8cfebfd0d760e296ed7fab5f3327fd
linxiao zeng <419344739@qq.com>
2019-11-14 19:31:21 +0100

    remove unsupported test in random sampling

--- onmt/tests/test_random_sampling.py
+++ onmt/tests/test_random_sampling.py
@@ -11,109 +11,6 @@ class TestRandomSampling(unittest.TestCase):
 
     BLOCKED_SCORE = -10e20
 
-    def test_advance_with_repeats_gets_blocked(self):
-        n_words = 100
-        repeat_idx = 47
-        ngram_repeat = 0
-        for batch_sz in [1, 3]:
-            samp = RandomSampling(
-                0, 1, 2, batch_sz, 0, ngram_repeat, set(),
-                False, 30, 1., 5)
-            samp._init_runtime(torch.device("cpu"),
-                               torch.randint(0, 30, (batch_sz,)))
-            for i in range(ngram_repeat + 4):
-                # predict repeat_idx over and over again
-                word_probs = torch.full(
-                    (batch_sz, n_words), -float('inf'))
-                word_probs[:, repeat_idx] = 0
-                attns = torch.randn(1, batch_sz, 53)
-                samp.advance(word_probs, attns)
-                if i <= ngram_repeat:
-                    expected_scores = torch.zeros((batch_sz, 1))
-                    self.assertTrue(samp.topk_scores.equal(expected_scores))
-                else:
-                    self.assertTrue(
-                        samp.topk_scores.equal(
-                            torch.tensor(self.BLOCKED_SCORE)
-                            .repeat(batch_sz, 1)))
-
-    def test_advance_with_some_repeats_gets_blocked(self):
-        # batch 0 and 7 will repeat, the rest will advance
-        n_words = 100
-        repeat_idx = 47
-        other_repeat_idx = 12
-        ngram_repeat = 0
-        for batch_sz in [1, 3, 13]:
-            samp = RandomSampling(
-                0, 1, 2, batch_sz, 0, ngram_repeat, set(),
-                False, 30, 1., 5)
-            samp._init_runtime(torch.device("cpu"),
-                               torch.randint(0, 30, (batch_sz,)))
-            for i in range(ngram_repeat + 4):
-                word_probs = torch.full(
-                    (batch_sz, n_words), -float('inf'))
-                # predict the same thing in batch 0 and 7 every i
-                word_probs[0, repeat_idx] = 0
-                if batch_sz > 7:
-                    word_probs[7, other_repeat_idx] = 0
-                # push around what the other batches predict
-                word_probs[1:7, repeat_idx + i] = 0
-                if batch_sz > 7:
-                    word_probs[8:, repeat_idx + i] = 0
-                attns = torch.randn(1, batch_sz, 53)
-                samp.advance(word_probs, attns)
-                if i <= ngram_repeat:
-                    self.assertFalse(
-                        samp.topk_scores.eq(
-                            self.BLOCKED_SCORE).any())
-                else:
-                    # now batch 0 and 7 die
-                    self.assertTrue(samp.topk_scores[0].eq(self.BLOCKED_SCORE))
-                    if batch_sz > 7:
-                        self.assertTrue(samp.topk_scores[7].eq(
-                            self.BLOCKED_SCORE))
-                    self.assertFalse(
-                        samp.topk_scores[1:7].eq(
-                            self.BLOCKED_SCORE).any())
-                    if batch_sz > 7:
-                        self.assertFalse(
-                            samp.topk_scores[8:].eq(
-                                self.BLOCKED_SCORE).any())
-
-    def test_repeating_excluded_index_does_not_die(self):
-        # batch 0 will repeat excluded idx, batch 1 will repeat
-        n_words = 100
-        repeat_idx = 47  # will be repeated and should be blocked
-        repeat_idx_ignored = 7  # will be repeated and should not be blocked
-        ngram_repeat = 0
-        for batch_sz in [1, 3, 17]:
-            samp = RandomSampling(
-                0, 1, 2, batch_sz, 0, ngram_repeat,
-                {repeat_idx_ignored}, False, 30, 1., 5)
-            samp._init_runtime(torch.device("cpu"),
-                               torch.randint(0, 30, (batch_sz,)))
-            for i in range(ngram_repeat + 4):
-                word_probs = torch.full(
-                    (batch_sz, n_words), -float('inf'))
-                word_probs[0, repeat_idx_ignored] = 0
-                if batch_sz > 1:
-                    word_probs[1, repeat_idx] = 0
-                    word_probs[2:, repeat_idx + i] = 0
-                attns = torch.randn(1, batch_sz, 53)
-                samp.advance(word_probs, attns)
-                if i <= ngram_repeat:
-                    self.assertFalse(samp.topk_scores.eq(
-                        self.BLOCKED_SCORE).any())
-                else:
-                    # now batch 1 dies
-                    self.assertFalse(samp.topk_scores[0].eq(
-                        self.BLOCKED_SCORE).any())
-                    if batch_sz > 1:
-                        self.assertTrue(samp.topk_scores[1].eq(
-                            self.BLOCKED_SCORE).all())
-                        self.assertFalse(samp.topk_scores[2:].eq(
-                            self.BLOCKED_SCORE).any())
-
     def test_doesnt_predict_eos_if_shorter_than_min_len(self):
         # batch 0 will always predict EOS. The other batches will predict
         # non-eos scores.
@@ -125,10 +22,9 @@ class TestRandomSampling(unittest.TestCase):
             min_length = 5
             eos_idx = 2
             lengths = torch.randint(0, 30, (batch_sz,))
-            ngram_repeat = 0
             samp = RandomSampling(
                 0, 1, 2, batch_sz, min_length,
-                ngram_repeat, set(), False, 30, 1., 1)
+                False, set(), False, 30, 1., 1)
             samp._init_runtime(torch.device("cpu"), lengths)
             all_attns = []
             for i in range(min_length + 4):
@@ -166,11 +62,10 @@ class TestRandomSampling(unittest.TestCase):
                 valid_score_dist_2 = torch.log_softmax(torch.tensor(
                     [6., 1.]), dim=0)
                 eos_idx = 2
-                ngram_repeat = 0
                 lengths = torch.randint(0, 30, (batch_sz,))
                 samp = RandomSampling(
                     0, 1, 2, batch_sz, 0,
-                    ngram_repeat, set(), False, 30, temp, 1)
+                    False, set(), False, 30, temp, 1)
                 samp._init_runtime(torch.device("cpu"), lengths)
                 # initial step
                 i = 0
@@ -239,11 +134,10 @@ class TestRandomSampling(unittest.TestCase):
                 valid_score_dist_2 = torch.log_softmax(torch.tensor(
                     [6., 1.]), dim=0)
                 eos_idx = 2
-                ngram_repeat = 0
                 lengths = torch.randint(0, 30, (batch_sz,))
                 samp = RandomSampling(
                     0, 1, 2, batch_sz, 0,
-                    ngram_repeat, set(), False, 30, temp, 2)
+                    False, set(), False, 30, temp, 2)
                 samp._init_runtime(torch.device("cpu"), lengths)
                 # initial step
                 i = 0
