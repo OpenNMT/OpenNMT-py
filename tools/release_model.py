2f02925d2ad6318166664f4c590349c878a7f1d3
Guillaume Klein <guillaume.klein@systrangroup.com>
2020-01-28 13:37:04 +0100

    Extend release script with CTranslate2 conversion

--- tools/release_model.py
+++ tools/release_model.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
-import argparse
-import torch
+from onmt.bin.release_model import main
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Removes the optim data of PyTorch models")
-    parser.add_argument("--model", "-m",
-                        help="The model filename (*.pt)", required=True)
-    parser.add_argument("--output", "-o",
-                        help="The output filename (*.pt)", required=True)
-    opt = parser.parse_args()
-
-    model = torch.load(opt.model)
-    model['optim'] = None
-    torch.save(model, opt.output)
+    main()
