c20dbeac02688918607637f5f30ec73c0f17d817
François Hernandez <francois.hernandez.fh@gmail.com>
2020-04-03 09:53:21 +0200

    [fix] Patch (old) fields when missing corpus_id (#1769)

--- onmt/train_single.py
+++ onmt/train_single.py
@@ -4,7 +4,7 @@ import os
 
 import torch
 
-from onmt.inputters.inputter import build_dataset_iter, \
+from onmt.inputters.inputter import build_dataset_iter, patch_fields, \
     load_old_vocab, old_style_vocab, build_dataset_iter_multiple
 from onmt.model_builder import build_model
 from onmt.utils.optimizers import Optimizer
@@ -68,6 +68,9 @@ def main(opt, device_id, batch_queue=None, semaphore=None):
             vocab, opt.model_type, dynamic_dict=opt.copy_attn)
     else:
         fields = vocab
+
+    # patch for fields that may be missing in old data/model
+    patch_fields(opt, fields)
 
     # Report src and tgt vocab sizes, including for features
     for side in ['src', 'tgt']:
