0982b90f9f36e2ad98f9de12b2666dd58e0a8a62
Marcel Gwerder <marcel.gwerder@thomsonreuters.com>
2020-06-03 15:29:47 +0200

    Pass logger when building translator

--- onmt/bin/translate.py
+++ onmt/bin/translate.py
@@ -15,7 +15,7 @@ def translate(opt):
     ArgumentParser.validate_translate_opts(opt)
     logger = init_logger(opt.log_file)
 
-    translator = build_translator(opt, report_score=True)
+    translator = build_translator(opt, logger=logger, report_score=True)
     src_shards = split_corpus(opt.src, opt.shard_size)
     tgt_shards = split_corpus(opt.tgt, opt.shard_size)
     shard_pairs = zip(src_shards, tgt_shards)
