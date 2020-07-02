91ab592b8982c5022c9feff6c13748d8c3fe0eb2
Linxiao ZENG <linxiao.zeng@gmail.com>
2020-07-02 23:31:45 +0800

    drop proba for nonchosen in target_prefix (#1823)

--- onmt/translate/decode_strategy.py
+++ onmt/translate/decode_strategy.py
@@ -214,17 +214,26 @@ class DecodeStrategy(object):
             pick_idx = self.target_prefix[:, step - 1].tolist()  # (B)
             pick_coo = [[path_i, pick] for path_i, pick in enumerate(pick_idx)
                         if pick not in [self.eos, self.pad]]
+            mask_pathid = [path_i for path_i, pick in enumerate(pick_idx)
+                           if pick in [self.eos, self.pad]]
             if len(pick_coo) > 0:
                 pick_coo = torch.tensor(pick_coo).to(self.target_prefix)
-                pick_fill_value = 10000 * torch.ones(
+                pick_fill_value = torch.ones(
                     [pick_coo.size(0)], dtype=log_probs.dtype)
                 # pickups: Tensor where specified index were set to 1, others 0
                 pickups = torch.sparse_coo_tensor(
                     pick_coo.t(), pick_fill_value,
                     size=log_probs.size(), device=log_probs.device).to_dense()
-                # Adding pickups to log_probs making probabilities of
-                # specified index close to 1
-                log_probs += pickups
+                # dropdowns: opposite of pickups, 1 for those shouldn't pick
+                dropdowns = torch.ones_like(pickups) - pickups
+                if len(mask_pathid) > 0:
+                    path_mask = torch.zeros(_B).to(self.target_prefix)
+                    path_mask[mask_pathid] = 1
+                    path_mask = path_mask.unsqueeze(1).to(dtype=bool)
+                    dropdowns = dropdowns.masked_fill(path_mask, 0)
+                # Minus dropdowns to log_probs making probabilities of
+                # unspecified index close to 0
+                log_probs -= 10000*dropdowns
         return log_probs
 
     def maybe_update_target_prefix(self, select_index):
