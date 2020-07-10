39178c4ccded2331d14b003a32820a173d4af16a
François Hernandez <francois.hernandez.fh@gmail.com>
2020-07-10 12:31:43 +0200

    remove useless line break

--- onmt/trainer.py
+++ onmt/trainer.py
@@ -297,15 +297,13 @@ class Trainer(object):
             if (self.model_saver is not None
                 and (save_checkpoint_steps != 0
                      and step % save_checkpoint_steps == 0)):
-                self.model_saver.save(
-                    step, moving_average=self.moving_average)
+                self.model_saver.save(step, moving_average=self.moving_average)
 
             if train_steps > 0 and step >= train_steps:
                 break
 
         if self.model_saver is not None:
-            self.model_saver.save(
-                step, moving_average=self.moving_average)
+            self.model_saver.save(step, moving_average=self.moving_average)
         return total_stats
 
     def validate(self, valid_iter, moving_average=None):
