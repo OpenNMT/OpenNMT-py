1b1e6c1ce5b29d09e87881797dce786148f51090
Linxiao ZENG <linxiao.zeng@gmail.com>
2020-10-23 11:48:06 +0200

    remove bias parameter from generator linear (#1903)

--- onmt/model_builder.py
+++ onmt/model_builder.py
@@ -162,7 +162,8 @@ def build_base_model(model_opt, fields, gpu, checkpoint=None, gpu_id=None):
             gen_func = nn.LogSoftmax(dim=-1)
         generator = nn.Sequential(
             nn.Linear(model_opt.dec_rnn_size,
-                      len(fields["tgt"].base_field.vocab)),
+                      len(fields["tgt"].base_field.vocab),
+                      bias=False),
             Cast(torch.float32),
             gen_func
         )
