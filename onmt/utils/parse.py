e347f5551f6d2f675a1c01cd6660fab2488b2f53
Ander Corral <a.corral@elhuyar.eus>
2021-04-07 16:31:39 +0200

    Allow --reset optim to be states or all

--- onmt/utils/parse.py
+++ onmt/utils/parse.py
@@ -290,7 +290,7 @@ class ArgumentParser(cfargparse.ArgumentParser, DataOptsCheckerMixin):
         if opt.update_vocab:
             assert opt.train_from, \
                 "-update_vocab needs -train_from option"
-            assert opt.reset_optim == 'states', \
+            assert opt.reset_optim in ['states', 'all'], \
                 '-update_vocab needs -reset_optim "states"'
 
     @classmethod
