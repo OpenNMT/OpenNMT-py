33a619a048cc8212e24a516483f9a308bd1b49a5
François Hernandez <francois.hernandez.fh@gmail.com>
2021-04-30 12:42:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='A python implementation of OpenNMT',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='2.1.0',
+    version='2.1.1',
     packages=find_packages(),
     project_urls={
         "Documentation": "http://opennmt.net/OpenNMT-py/",
