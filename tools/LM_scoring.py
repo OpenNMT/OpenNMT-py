18da936937f5638be40f3be212f117855c541dc1
vince62s <vince62s@yahoo.com>
2022-09-09 13:43:52 +0200

    simplify in one script and clean

--- tools/LM_scoring.py
+++ tools/LM_scoring.py
@@ -11,12 +11,28 @@ from onmt.inputters.text_dataset import InferenceDataIterator, \
 from onmt.transforms import make_transforms, get_transforms_cls, TransformPipe
 from onmt.model_builder import load_test_model
 
-"""This script computes the ppl of a file using dynamic data."""
+"""This script scores all sentences of a file using dynamic data."""
 """For this purpose we use the same pipeline as the validation of a file"""
-
+"""Below is an example of settings of a config.yaml file"""
+"""
+model: lm-de.news2021_step_100000.pt
+src: newstest2014-ref.de
+tgt: newstest2014-ref.de
+transforms: [onmt_tokenize]
+batch_size: 16
+gpu: 0
+src_subword_type: bpe
+src_subword_model: subwords.en_de.bpe
+src_onmttok_kwargs: '{"mode": "aggressive"}'
+tgt_subword_type: bpe
+tgt_subword_model: subwords.en_de.bpe
+tgt_onmttok_kwargs: '{"mode": "aggressive"}'
+"""
+"""Output is the data and tab separated score"""
+"""Corpus PPL is in the logger.info"""
 
 def _get_parser():
-    parser = ArgumentParser(description='corpus_ppl.py')
+    parser = ArgumentParser(description='LM_scoring.py')
     opts.config_opts(parser)
     opts.translate_opts(parser, dynamic=True)
     return parser
@@ -56,11 +72,9 @@ def main():
     data_reader = InferenceDataReader(opt.src, opt.tgt, opt.src_feats)
 
     tgt_field = dict(fields)["tgt"].base_field
-
-    # We could use build_loss_compute without reduction 'none' in the criterion
-    # to get the loss of each sentence instead of the loss of the full batch
-    # However this is more in line with the other script score_sentences.py
     padding_idx = tgt_field.vocab.stoi[tgt_field.pad_token]
+    # Cannot use build_loss_compute() we need reduction 'none' in the criterion
+    # to get the loss of each sentence instead of the loss of the full batch
     criterion = torch.nn.NLLLoss(ignore_index=padding_idx, reduction='none')
     loss_gen = model.generator
     valid_loss = LMLossCompute(criterion, loss_gen,
@@ -70,7 +84,7 @@ def main():
 
     cumul_loss = 0.0
     cumul_length = 0
-
+    # Now we can pipe the full file through the model using the Iterator
     with torch.no_grad():
         for i, (src_shard, tgt_shard, feats_shard) in enumerate(data_reader):
             logger.info("Translating shard %d." % i)
@@ -92,22 +106,32 @@ def main():
                 sort_within_batch=True,
                 shuffle=False,
             )
-
             for i, batch in enumerate(data_iter2):
+                # reminder a batch includes .src .tgt .indices and it is sorted
+                batch_size = len(batch)
                 src, src_lengths = batch.src if isinstance(batch.src, tuple) \
                     else (batch.src, None)
                 tgt = batch.tgt
                 outputs, attns = model(src, tgt, src_lengths,
                                        with_align=False)
-                # Compute loss.
-                loss, stats = valid_loss(batch, outputs, attns)
-                batch_loss = loss.sum().item()
-                batch_length = src_lengths.sum().cpu().numpy()
-                cumul_loss += batch_loss
-                cumul_length += batch_length
-        avg_loss = cumul_loss / cumul_length
-        print("loss: ", cumul_loss, " length: ", cumul_length, " avg loss ",
-              avg_loss, "Corpus PPL: ", np.exp(avg_loss))
+                # Compute and retrieve the loss for EACH sentence
+                lossflat, _ = valid_loss(batch, outputs, attns)
+                loss = lossflat.view(-1, batch_size)
+                mask = (loss != 0)
+                sent_loss = torch.sum(loss, dim=0) / mask.sum(dim=0)
+                sent_ppl = torch.exp(sent_loss)
+                cumul_loss += loss.sum().item()
+                cumul_length += mask.sum().cpu()
+                # Now we need to rearrange the batch of ppl
+                # in the original order with indices
+                sent_ppl_orig = sent_ppl.gather(0, batch.indices.argsort(0))
+                for j in range(batch_size):
+                    srctxt = src_shard[i * opt.batch_size + j]
+                    print(srctxt.strip().decode("UTF-8"),
+                          "\t", sent_ppl_orig[j].item())
+        logger.info("Loss: %.2f Tokens: %d Corpus PPL: %.2f" %
+                    (cumul_loss, cumul_length,
+                     np.exp(cumul_loss / cumul_length)))
 
 
 if __name__ == "__main__":
