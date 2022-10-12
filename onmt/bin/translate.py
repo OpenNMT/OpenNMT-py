a929b1fc2257e309ded70382cf3c298848d85f2d
vince62s <vince62s@yahoo.com>
2022-10-12 16:18:35 +0200

    remove debug print

--- onmt/bin/translate.py
+++ onmt/bin/translate.py
@@ -35,14 +35,12 @@ def translate(opt):
     ]
     transform = TransformPipe.build_from(data_transform)
 
-    all_scores, all_preds = translator._translate(
+    _, _ = translator._translate(
         infer_iter,
         transform=transform,
         attn_debug=opt.attn_debug,
         align_debug=opt.align_debug
         )
-    print(all_scores)
-    print(all_preds)
 
 
 def _get_parser():
