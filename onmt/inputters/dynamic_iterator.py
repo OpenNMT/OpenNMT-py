e42c209a6aeb5ec55ec7e7f71f10557f7cf60121
l-k-11235 <linakhodja@gmail.com>
2022-10-26 16:31:40 +0200

    make prefetch_factor configurable in DataLoader

--- onmt/inputters/dynamic_iterator.py
+++ onmt/inputters/dynamic_iterator.py
@@ -309,5 +309,15 @@ def build_dynamic_dataset_iter(opt, transforms_cls, vocabs, copy=False,
                                  pin_memory=True,
                                  multiprocessing_context="fork",
                                  num_workers=data_iter.num_workers,
-                                 worker_init_fn=data_iter._init_datasets)
+                                 worker_init_fn=data_iter._init_datasets,
+                                 prefetch_factor=opt.prefetch_factor)
+        # We disable automatic batching in the DataLoader.
+        # The custom optimized batching is performed by the
+        # custom class DynamicDatasetIter inherited from IterableDataset
+        # (and not by a custom collate function).
+        # We load opt.bucket_size examples, sort them and yield
+        # mini-batchs of size opt.batch_size.
+        # The bucket_size must be large enough to ensure homogeneous batches.
+        # Each worker will load opt.prefetch_factor mini-batches in
+        # advance to avoid the GPU waiting during the refilling of the bucket.
     return data_loader
