cddf6f7e4e63f562b5b8af753da5cb7ec115f42d
l-k-11235 <linakhodja@gmail.com>
2022-11-22 18:32:21 +0100

    moved the utils.py file from /translate to /utils and changed the name to scoring_utils.py

--- onmt/trainer.py
+++ onmt/trainer.py
@@ -15,7 +15,7 @@ import traceback
 import onmt.utils
 from onmt.utils.loss import LossCompute
 from onmt.utils.logging import logger
-from onmt.translate.utils import ScoringPreparator
+from onmt.utils.scoring_utils import ScoringPreparator
 from onmt.scorers import get_scorers_cls, build_scorers
 
 
