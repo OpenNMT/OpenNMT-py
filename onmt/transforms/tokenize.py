ff9effde0304afa3292c5582a44ea85b0de3cabf
Vincent Nguyen <vince62s@yahoo.com>
2022-12-16 08:31:28 +0100

    fix no tgt at inference (#2273)

--- onmt/transforms/tokenize.py
+++ onmt/transforms/tokenize.py
@@ -194,11 +194,18 @@ class SentencePieceTransform(TokenizerTransform):
     def apply(self, example, is_train=False, stats=None, **kwargs):
         """Apply sentencepiece subword encode to src & tgt."""
         src_out = self._tokenize(example['src'], 'src', is_train)
-        tgt_out = self._tokenize(example['tgt'], 'tgt', is_train)
-        if stats is not None:
-            n_words = len(example['src']) + len(example['tgt'])
-            n_subwords = len(src_out) + len(tgt_out)
-            stats.update(SubwordStats(n_subwords, n_words))
+        if example['tgt'] is not None:
+            tgt_out = self._tokenize(example['tgt'], 'tgt', is_train)
+            if stats is not None:
+                n_words = len(example['src']) + len(example['tgt'])
+                n_subwords = len(src_out) + len(tgt_out)
+                stats.update(SubwordStats(n_subwords, n_words))
+        else:
+            tgt_out = None
+            if stats is not None:
+                n_words = len(example['src'])
+                n_subwords = len(src_out)
+                stats.update(SubwordStats(n_subwords, n_words))
         example['src'], example['tgt'] = src_out, tgt_out
         return example
 
@@ -279,11 +286,18 @@ class BPETransform(TokenizerTransform):
     def apply(self, example, is_train=False, stats=None, **kwargs):
         """Apply bpe subword encode to src & tgt."""
         src_out = self._tokenize(example['src'], 'src', is_train)
-        tgt_out = self._tokenize(example['tgt'], 'tgt', is_train)
-        if stats is not None:
-            n_words = len(example['src']) + len(example['tgt'])
-            n_subwords = len(src_out) + len(tgt_out)
-            stats.update(SubwordStats(n_subwords, n_words))
+        if example['tgt'] is not None:
+            tgt_out = self._tokenize(example['tgt'], 'tgt', is_train)
+            if stats is not None:
+                n_words = len(example['src']) + len(example['tgt'])
+                n_subwords = len(src_out) + len(tgt_out)
+                stats.update(SubwordStats(n_subwords, n_words))
+        else:
+            tgt_out = None
+            if stats is not None:
+                n_words = len(example['src'])
+                n_subwords = len(src_out)
+                stats.update(SubwordStats(n_subwords, n_words))
         example['src'], example['tgt'] = src_out, tgt_out
         return example
 
