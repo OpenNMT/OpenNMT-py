cfc45b7890235c5157ad00f0619ac7d3f76858ac
vince62s <vince62s@yahoo.com>
2022-12-20 12:47:01 +0100

    flake

--- examples/wmt17/filter_train.py
+++ examples/wmt17/filter_train.py
@@ -8,7 +8,8 @@ if len(sys.argv[1:]) >= 4:
     in_src_fname, in_trg_fname, out_src_fname, out_trg_fname = sys.argv[1:5]
 else:
     in_src_fname, in_trg_fname, out_src_fname, out_trg_fname = \
-        'train.src.bpe', 'train.trg.bpe', 'train.src.bpe.filter', 'train.trg.bpe.filter'
+        'train.src.bpe', 'train.trg.bpe', \
+        'train.src.bpe.filter', 'train.trg.bpe.filter'
 
 max_tokens = int(sys.argv[5]) if len(sys.argv[1:]) > 4 else 95
 
@@ -19,7 +20,7 @@ with open(in_src_fname, mode='rt', encoding='utf-8') as in_src, \
 
     for src_line, trg_line in zip(in_src, in_trg):
 
-        if len(src_line.split()) <= max_tokens and len(trg_line.split()) <= max_tokens:
+        if (len(src_line.split()) <= max_tokens and
+                len(trg_line.split()) <= max_tokens):
             print(src_line, file=out_src, end='')
             print(trg_line, file=out_trg, end='')
-
