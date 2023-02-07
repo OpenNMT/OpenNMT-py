1f8b7028b2eb151d8d1d00313ca49d74395f6460
anderleich <andercorral95@gmail.com>
2023-02-07 11:12:28 +0100

    Fixed inferfeats transform

--- onmt/transforms/features.py
+++ onmt/transforms/features.py
@@ -1,7 +1,6 @@
 from onmt.transforms import register_transform
 from .transform import Transform
 from onmt.utils.alignment import subword_map_by_joiner, subword_map_by_spacer
-import re
 
 
 @register_transform(name='inferfeats')
@@ -19,14 +18,10 @@ class InferFeatsTransform(Transform):
                   default="joiner", choices=["joiner", "spacer"],
                   help="Type of reversible tokenization "
                        "applied on the tokenizer.")
-        group.add("--prior_tokenization", "-prior_tokenization",
-                  default=False, action="store_true",
-                  help="Whether the input has already been tokenized.")
 
     def _parse_opts(self):
         super()._parse_opts()
         self.reversible_tokenization = self.opts.reversible_tokenization
-        self.prior_tokenization = self.opts.prior_tokenization
 
     def apply(self, example, is_train=False, stats=None, **kwargs):
 
@@ -35,8 +30,7 @@ class InferFeatsTransform(Transform):
             return example
 
         if self.reversible_tokenization == "joiner":
-            original_src = example["src_original"] \
-                if self.prior_tokenization else None
+            original_src = example["src_original"]
             word_to_subword_mapping = subword_map_by_joiner(
                 example["src"], original_subwords=original_src)
         else:  # Spacer
@@ -45,13 +39,7 @@ class InferFeatsTransform(Transform):
         new_src_feats = [[] for _ in range(len(example["src_feats"]))]
         for subword, word_id in zip(example["src"], word_to_subword_mapping):
             for i, feat_values in enumerate(example["src_feats"]):
-                # Punctuation only
-                if not re.sub(r'(\W)+', '', subword).strip() \
-                        and not self.prior_tokenization:
-                    inferred_feat = "<null>"
-                else:
-                    inferred_feat = feat_values[word_id]
-
+                inferred_feat = feat_values[word_id]
                 new_src_feats[i].append(inferred_feat)
         example["src_feats"] = new_src_feats
 
