82a16a0d9b434cf9174876dd3d8b03464a8cb348
vince62s <vince62s@yahoo.com>
2023-06-13 11:27:30 +0200

    lora_weights merging

--- onmt/models/model.py
+++ onmt/models/model.py
@@ -2,7 +2,6 @@
 import torch
 import torch.nn as nn
 import glob
-from itertools import chain
 
 
 class BaseModel(nn.Module):
--- tools/lora_weights.py
+++ tools/lora_weights.py
@@ -1,9 +1,12 @@
 import torch
 import argparse
-from onmt.utils.logging import init_logger
+from onmt.utils.logging import init_logger, logger
 from onmt.models.model_saver import load_checkpoint
 from onmt.inputters.inputter import dict_to_vocabs, vocabs_to_dict
 from onmt.model_builder import build_base_model
+from safetensors import safe_open
+from safetensors.torch import save_file
+import glob
 
 """
     This script merges or concat LoRa weights into the main model
@@ -38,6 +41,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output", type=str, required=True, help="""Path to the output model"""
     )
+    parser.add_argument(
+        "--format",
+        type=str,
+        choices=["pytorch", "safetensors"],
+        default="pytorch",
+        help="""Format to save the output model""",
+    )
+
     opt = parser.parse_args()
 
     init_logger()
@@ -50,42 +61,97 @@ if __name__ == "__main__":
 
     lora_opt = lora_checkpoint["opt"]
 
-    lora_opt.quant_layers = []
+    lora_opt.quant_layers = []  # we need to remove any quantization to merge weights
 
     model = build_base_model(lora_opt, vocabs)
 
-    model.load_state_dict(base_checkpoint, strict=False)
-    model.load_state_dict(lora_checkpoint, strict=False)
+    if "model" in base_checkpoint.keys():
+        model.load_state_dict(
+            base_checkpoint,
+            precision=torch.float32,
+            device=torch.device("cpu"),
+            strict=False,
+        )
+    else:
+        model.load_safe_state_dict(
+            opt.base_model,
+            precision=torch.float32,
+            device=torch.device("cpu"),
+            strict=False,
+        )
+
+    if "model" in lora_checkpoint.keys():
+        model.load_state_dict(
+            lora_checkpoint,
+            precision=torch.float32,
+            device=torch.device("cpu"),
+            strict=False,
+        )
+    else:
+        model.load_safe_state_dict(
+            opt.lora_weights,
+            precision=torch.float32,
+            device=torch.device("cpu"),
+            strict=False,
+        )
 
     if opt.action == "merge":
         model.eval()  # this merges automatically LoRa weights in main
         model.half()  # We keep FP16 for all
         optim = None
         model_state_dict = model.state_dict()
-        model_state_dict = {
-            k: v
-            for k, v in model_state_dict.items()
-            if "generator" not in k and "lora" not in k
-        }
+        if opt.format == "pytorch":
+            model_state_dict = {
+                k: v
+                for k, v in model_state_dict.items()
+                if "generator" not in k and "lora" not in k
+            }
         new_opt = base_checkpoint["opt"]
     elif opt.action == "concat":
         model.half()  # We keep FP16 for all
         optim = lora_checkpoint["optim"]
         model_state_dict = model.state_dict()
-        model_state_dict = {
-            k: v for k, v in model_state_dict.items() if "generator" not in k
-        }
+        if opt.format == "pytorch":
+            model_state_dict = {
+                k: v for k, v in model_state_dict.items() if "generator" not in k
+            }
         new_opt = lora_opt
     else:
         raise ValueError("action not supported, please choose merge or concat")
-
-    generator_state_dict = model.generator.state_dict()
-
-    new_checkpoint = {
-        "model": model_state_dict,
-        "generator": generator_state_dict,
-        "vocab": vocabs_to_dict(vocabs),
-        "opt": new_opt,
-        "optim": optim,
-    }
-    torch.save(new_checkpoint, opt.output)
+    if opt.format == "pytorch":
+        generator_state_dict = model.generator.state_dict()
+        new_checkpoint = {
+            "model": model_state_dict,
+            "generator": generator_state_dict,
+            "vocab": vocabs_to_dict(vocabs),
+            "opt": new_opt,
+            "optim": optim,
+        }
+        logger.info("Saving merged model")
+        if opt.output[-3:] == ".pt":
+            torch.save(new_checkpoint, opt.output)
+        else:
+            torch.save(new_checkpoint, opt.output + ".pt")
+    elif opt.format == "safetensors":
+        new_checkpoint = {
+            "vocab": vocabs_to_dict(vocabs),
+            "opt": new_opt,
+            "optim": optim,
+        }
+        if opt.output[-3:] == ".pt":
+            torch.save(new_checkpoint, opt.output)
+        else:
+            torch.save(new_checkpoint, opt.output + ".pt")
+        fileout = opt.output[:-3] if opt.output[-3:] == ".pt" else opt.output
+        basepath = (
+            opt.base_model[:-3] if opt.base_model[-3:] == ".pt" else opt.base_model
+        )
+        shards = glob.glob(basepath + ".*.safetensors")
+        f = []
+        for i, shard in enumerate(shards):
+            shard_dict = {}
+            f.append(safe_open(shard, framework="pt", device="cpu"))
+            for key in f[i].keys():
+                shard_dict[key] = model_state_dict[key]
+            logger.info("saving shard", fileout + ".{:02d}.safetensors".format(i))
+            save_file(shard_dict, fileout + ".{:02d}.safetensors".format(i))
