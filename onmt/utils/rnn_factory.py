03f96cc741154aa861e30f50b497523f2d9de054
Wonhee Go <50973836+stellarway@users.noreply.github.com>
2023-07-14 19:40:58 +0900

    DEBUG: `SRU` class was moved to `onmt.modules`
    Update rnn_factory.py

--- onmt/utils/rnn_factory.py
+++ onmt/utils/rnn_factory.py
@@ -2,7 +2,7 @@
  RNN tools
 """
 import torch.nn as nn
-import onmt.models
+import onmt.modules
 
 
 def rnn_factory(rnn_type, **kwargs):
@@ -11,7 +11,7 @@ def rnn_factory(rnn_type, **kwargs):
     if rnn_type == "SRU":
         # SRU doesn't support PackedSequence.
         no_pack_padded_seq = True
-        rnn = onmt.models.sru.SRU(batch_first=True, **kwargs)
+        rnn = onmt.modules.sru.SRU(batch_first=True, **kwargs)
     else:
         rnn = getattr(nn, rnn_type)(batch_first=True, **kwargs)
     return rnn, no_pack_padded_seq
