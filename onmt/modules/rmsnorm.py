ea900d88daac9de3a6823ee2b4e288299a03416e
vince62s <vince62s@yahoo.com>
2023-12-19 16:57:35 +0100

    black is black

--- onmt/modules/rmsnorm.py
+++ onmt/modules/rmsnorm.py
@@ -26,12 +26,12 @@ class RMSNorm(torch.nn.Module):
     def forward(self, hidden_states):
         if AWQ_INFERENCE_ENGINE:
             output = torch.empty_like(hidden_states)
-            if hidden_states.dim() == 2: # patch for multi experts
+            if hidden_states.dim() == 2:  # patch for multi experts
                 hidden_states = hidden_states.unsqueeze(0)
             awq_inference_engine.layernorm_forward_cuda(
                 hidden_states, self.weight, output, self.eps
             )
-            if hidden_states.dim() == 2: # patch for multi experts
+            if hidden_states.dim() == 2:  # patch for multi experts
                 output = output.unsqueeze(0)
             return output
         else:
